27b9cd81a78f958b1c31a6a0a0c63e64ffe18f00
Victor Mylle <victor.mylle@hotmail.com>
2022-02-28 09:40:52 +0000

    added UserLogin model

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,49 @@
 from fastapi import FastAPI
-from .routers import users
+from .routers import users,auth
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(auth.router)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+
+    # Custom documentation fastapi-jwt-auth
+    headers = {
+        "name": "Authorization",
+        "in": "header",
+        "required": True,
+        "schema": {
+            "title": "Authorization",
+            "type": "string"
+        },
+    }
+
+    # Get routes from index 4 because before that fastapi define router for /openapi.json, /redoc, /docs, etc
+    # Get all router where operation_id is authorize
+    router_authorize = [route for route in app.routes[4:] if route.operation_id == "authorize"]
+
+    for route in router_authorize:
+        method = list(route.methods)[0].lower()
+        try:
+            # If the router has another parameter
+            openapi_schema["paths"][route.path][method]['parameters'].append(headers)
+        except Exception:
+            # If the router doesn't have a parameter
+            openapi_schema["paths"][route.path][method].update({"parameters":[headers]})
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, Field
 class User(BaseModel):
     username: str = Field(index=True)
     password: str = Field(...)
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
