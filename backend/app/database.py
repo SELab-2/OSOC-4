c92a0678131db15f5f941fb07578ba7ac365d48b
Victor Mylle <victor.mylle@hotmail.com>
2022-03-06 17:10:02 +0000

    added docker-compose file

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,13 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
+import os
 
-MONGO_DETAILS = "mongodb://root:justapassword@192.168.0.102:27017"
+MONGO_URL = os.environ.get('MONGO_URL')
+MONGO_PORT = os.environ.get('MONGO_PORT')
+MONGO_USER = os.environ.get('MONGO_USER')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+
+MONGO_DETAILS = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URL}:{MONGO_PORT}"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
