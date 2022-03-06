9a1dac0b3ca7212d8d2810ab707ae8687d831268
Victor Mylle <victor.mylle@hotmail.com>
2022-03-06 16:33:01 +0000

    added response_model to endpoints

--- backend/app/routers/studentforms.py
+++ backend/app/routers/studentforms.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 from app.utils.response import response, errorresponse
 from app.utils.tallyhandler import process_tally
 from app.crud.student_forms import get_all_studentforms, get_studentform_with_id
-from app.models.student_form import StudentFormOut
+from app.models.student_form import StudentForm, StudentFormOut
+from typing import List
+
 
 router = APIRouter(prefix="/studentforms")
 
@@ -13,13 +15,13 @@ async def get_all_form_ids():
     return response([str(form.id) for form in forms], "Fetched all forms successfully")
 
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=StudentForm)
 async def get_form_with_id(id):
     form = await get_studentform_with_id(id)
     return response(form, "Fetched one form")
 
 
-@router.get("/simple")
+@router.get("/simple", response_model=List[StudentFormOut])
 async def get_forms_basic():
     forms = []
     ffen = await get_all_studentforms()
