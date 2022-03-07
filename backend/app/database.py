4dd41c11ed538669c3b334b436be02b57a2bc683
Victor Mylle <victor.mylle@hotmail.com>
2022-03-07 11:52:50 +0000

    using local .env file for development

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,14 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 import os
+from dotenv import load_dotenv
 
-MONGO_URL = os.environ.get('MONGO_URL')
-MONGO_PORT = os.environ.get('MONGO_PORT')
-MONGO_USER = os.environ.get('MONGO_USER')
-MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+load_dotenv()
+
+MONGO_URL = os.getenv('MONGO_URL')
+MONGO_PORT = os.getenv('MONGO_PORT')
+MONGO_USER = os.getenv('MONGO_USER')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 
 MONGO_DETAILS = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URL}:{MONGO_PORT}"
 
