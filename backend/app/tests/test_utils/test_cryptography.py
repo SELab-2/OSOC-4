82bfa287a357f881e5695bf8de2432a57e7aa465
StefVdHaute <vdhstef@hotmail.com>
2022-03-07 23:21:28 +0100

    cryptography testing

--- backend/app/tests/test_utils/test_cryptography.py
+++ backend/app/tests/test_utils/test_cryptography.py
@@ -1,11 +1,19 @@
 import unittest
-from app.utils.cryptography import get_password_hash
+from app.utils.cryptography import get_password_hash, verify_password
 
 
 class TestCryptography(unittest.TestCase):
+    test_password = "I'm a super safe password"
 
     def test_get_password_hash(self):
-        password = "I'm a super safe password"
-        hash1 = get_password_hash(password)
-        hash2 = get_password_hash(password)
-        self.assertNotEqual(hash1, hash2)
+        self.assertNotEqual(self.test_password, get_password_hash(self.test_password),
+                            "Password hash was equal to password.")
+
+    def test_password_verification(self):
+        self.assertTrue(verify_password(self.test_password, get_password_hash(self.test_password)),
+                        "Password was not verified.")
+
+    def test_password_salt(self):
+        hash1 = get_password_hash(self.test_password)
+        hash2 = get_password_hash(self.test_password)
+        self.assertNotEqual(hash1, hash2, "Password hashes were equal.")
