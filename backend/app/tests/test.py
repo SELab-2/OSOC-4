3c0017852dc57ac2c73e7c6a4a1408ca759e8bd4
Victor Mylle <victor.mylle@hotmail.com>
2022-03-08 17:24:17 +0000

    Removed some testfiles

--- backend/app/tests/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    loader = unittest.TestLoader()
-    start_dir = '.'
-    suite = loader.discover(start_dir)
-
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
