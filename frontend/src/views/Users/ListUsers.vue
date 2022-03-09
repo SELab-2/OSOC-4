128df769d7c486696c7721c20c7a362f6a4d9ad7
tihabils <tibe.habils@ugent.be>
2022-03-10 00:13:58 +0100

    Added basic listing capability for users.

---
+++ frontend/src/views/Users/ListUsers.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+import ItemUser from "@/views/Users/ItemUser.vue";
+
+let users = [{name: "Test", studies: "Computer science"}]
+</script>
+
+<template>
+  <h2>Users</h2>
+  <div v-for="user in users">
+    <item-user :user="user"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
