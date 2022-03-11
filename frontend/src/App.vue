60b203b35453627350a41e65ab7f35f90aa2472d
tihabils <tibe.habils@ugent.be>
2022-03-11 14:58:20 +0100

    removed admin test mode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,28 +4,19 @@
   import NotLoggedInHeader from "@/views/headers/NotLoggedInHeader.vue";
 
   const loggedIn = ref(false)
-  const admin = ref(false)
   function switchLoginStatus() {
     loggedIn.value = ! loggedIn.value
   }
-  function switchAdminStatus(){
-    admin.value = ! admin.value
-  }
-
 </script>
 
 <template>
   <header>
   </header>
   <main>
-    <p v-if=admin.valueOf()> you are an admin </p>
     <LoggedInHeader v-if="loggedIn"/>
-    <template v-else>
-      <NotLoggedInHeader :admin="admin.valueOf()"/>
-    </template>
+    <NotLoggedInHeader v-else/>
     <br>
     <button @click=switchLoginStatus()>Log off / on view</button>
-    <button @click=switchAdminStatus()>Turn admin mode off / on</button>
   </main>
 </template>
 
