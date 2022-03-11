7a56dd4101dc82597c2a50a55771969e9e9a1e6d
tihabils <tibe.habils@ugent.be>
2022-03-11 11:47:34 +0100

    remove signup

--- frontend/src/views/headers/NotLoggedInHeader.vue
+++ frontend/src/views/headers/NotLoggedInHeader.vue
@@ -3,7 +3,6 @@
 </script>
 
 <template>
-  <router-link to="/sign-up" class="nav-link">Sign Up</router-link>
   <router-link to="/login" class="nav-link">Login</router-link>
   <router-view/>
 </template>
