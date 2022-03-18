aad2f012aad35bb6ae00d8782a67e50ac585968a
Quinten Vervynck <vervynck.quinten@gmail.com>
2022-03-18 20:42:42 +0100

    added forgot & some basic styling

--- frontend_react/src/Components/Login.js
+++ frontend_react/src/Components/Login.js
@@ -2,12 +2,16 @@ import React, {useState} from "react";
 import {useLocation, useNavigate} from 'react-router-dom'
 import {login} from "../utils/json-requests";
 import {log} from "../utils/logger";
+import "../styles/colors.css"
+import "../styles/login.css"
 
 
 
 const Login = props => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showForgot, setShowForgot] = useState(false)
+    const [emailForgot, setEmailForgot] = useState("")
     let from =  useLocation().state?.from?.pathname || "/";
     const navigate = useNavigate()
 
@@ -19,7 +23,12 @@ const Login = props => {
         setPassword(event.target.value);
     }
 
-    async function handleSubmit(event) {
+    const handleChangeEmailForgot = (event) => {
+        setEmailForgot(event.target.value);
+    }
+
+
+    async function handleSubmitLogin(event) {
         log("handle login submit")
         event.preventDefault();
         let credentials = JSON.stringify({
@@ -37,21 +46,54 @@ const Login = props => {
         }
     }
 
+    async function handleSubmitForgot(event) {
+        log("handle forgot submit")
+        event.preventDefault();
+        let credentials = JSON.stringify({
+            "email": email,
+        });
+        log(credentials)
+        // post, if any errors, show them
+        let output = await login("/forgot", credentials);
+        console.log(output)
+        if (output.success) {
+        }
+        setShowForgot(false)
+    }
+
     return (
-            <div className="body">
+        <div className="body">
+            <section className="body-left">
+                <img src={process.env.PUBLIC_URL + "/assets/0-1-osoc-full-2.png"} alt="osoc-logo"/>
+            </section>
+
+            <section className="body-right">
                 <div className="login-container">
                     <p className="welcome-message">Please provide login credentials to proceed</p>
                     <div className="login-form">
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmitLogin}>
                             <input type="email" name="email" value={email} onChange={handleChangeEmail} placeholder="Email address"/>
                             <input type="password" name="password" value={password} onChange={handleChangePassword} placeholder="Password"/>
                             <input className="submit" type="submit" name="submit" value="Login"/>
                         </form>
                     </div>
+                    {(!showForgot)? (
+                        <button className="submit" onClick={() => {setEmailForgot(email); setShowForgot(true);}} >Forgot your password?</button>
+                        ):
+                        (
+                        <div>
+                            <p className="welcome-message">Please provide your email address to proceed</p>
+                            <form onSubmit={handleSubmitForgot}>
+                                <input type="email" name="email" value={emailForgot} onChange={handleChangeEmailForgot} placeholder="Email address"/>
+                                <input className="submit" type="submit" name="submit" value="Reset my password"/>
+                            </form>
+                        </div>
+                        )}
                 </div>
-            </div>
-        )
-    }
+            </section>
+        </div>
+    )
+}
 
 
 
