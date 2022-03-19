03ac03feeafa87c16605033a6d167fb8e43d9f20
Victor Mylle <victor.mylle@hotmail.com>
2022-03-19 16:04:06 +0000

    added base to BrowserRouter

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
