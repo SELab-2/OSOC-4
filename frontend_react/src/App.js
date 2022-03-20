ad9ea34c02e429b2630c57409dce058a9d916f8e
Victor Mylle <victor.mylle@hotmail.com>
2022-03-20 11:25:37 +0000

    Cleaned up routing

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Login from './Components/Login'
-import { Route, Routes} from 'react-router-dom'
-import {useEffect, useState} from "react";
+import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from "react";
 import NavHeader from './Components/NavHeader.js'
 import SelectUsers from "./Components/SelectUsers";
 import Projects from "./Components/Projects";
@@ -9,7 +9,7 @@ import RequireAuthentication from "./Components/authentication/RequireAuthentica
 import EmailUsers from "./Components/EmailUsers";
 import Settings from "./Components/Settings";
 import ErrorPage from "./Components/ErrorPage";
-import {getJson, isStillAuthenticated} from "./utils/json-requests";
+import { getJson, isStillAuthenticated } from "./utils/json-requests";
 
 function App() {
     let [loggedInAs, setLoggedInAs] = useState(null);
@@ -21,40 +21,24 @@ function App() {
     }});
      */
 
+    if (!loggedInAs) {
+        return <Login setLoggedInAs={setLoggedInAs} />
+    }
+
     return (
-    <div className="App">
-      <Routes>
-        <Route path='/login' element={<Login setLoggedInAs={setLoggedInAs} />} />
-        <Route path='/select-users' element={
-            <RequireAuthentication loggedInAs={loggedInAs} setLoggedInAs={setLoggedInAs}>
-                <NavHeader setLoggedInAs={setLoggedInAs} />
-                <SelectUsers/>
-            </RequireAuthentication>
-        }/>
-        <Route path='/email-users' element={
-            <RequireAuthentication loggedInAs={loggedInAs} setLoggedInAs={setLoggedInAs}>
-                <NavHeader setLoggedInAs={setLoggedInAs} />
-                <EmailUsers/>
-            </RequireAuthentication>
-        }/>
-        <Route path='/projects' element={
-            <RequireAuthentication loggedInAs={loggedInAs} setLoggedInAs={setLoggedInAs}>
-                <NavHeader setLoggedInAs={setLoggedInAs} />
-                <Projects/>
-            </RequireAuthentication>
-        }/>
-        <Route path='/settings' element={
-            <RequireAuthentication loggedInAs={loggedInAs} setLoggedInAs={setLoggedInAs}>
-                <NavHeader setLoggedInAs={setLoggedInAs} />
-                <Settings loggedInAs={loggedInAs}/>
-            </RequireAuthentication>
-        }/>
-        <Route path="*" element={
-            <ErrorPage status={404} message={"Page not found"}/>
-        }/>
-      </Routes>
-    </div>
-  );
+        <div className="App">
+            <NavHeader setLoggedInAs={setLoggedInAs} />
+            <Routes>
+                <Route path='/select-users' element={<SelectUsers />} />
+                <Route path='/email-users' element={<EmailUsers />} />
+                <Route path='/projects' element={<Projects />} />
+                <Route path='/settings' element={<Settings loggedInAs={loggedInAs} />} />
+                <Route path="*" element={
+                    <ErrorPage status={404} message={"Page not found"} />
+                } />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
