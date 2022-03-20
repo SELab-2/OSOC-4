7265e4ce2aa0d7afd025380fffed1833bc686750
Simon Van de Vyver <simon.vandevyver@ugent.be>
2022-03-20 09:55:40 +0100

    students listelement verder

--- frontend_react/src/Components/SelectUsers.js
+++ frontend_react/src/Components/SelectUsers.js
@@ -1,9 +1,19 @@
 import StudentListelement from "./StudentListelement";
+import {useEffect, useState} from "react";
 
 export default function SelectUsers(props) {
+
+  const [students, setStudents] = useState(undefined);
+
+  useEffect(
+    if (!students) {
+      getJson(props.)
+    }
+  )
+
     return(
         <div className="students-list">
-            <StudentListelement/>
+            <StudentListelement student={}/>
         </div>
     )
 }
--- frontend_react/src/Components/StudentListelement.js
+++ frontend_react/src/Components/StudentListelement.js
@@ -1,9 +1,27 @@
+import {useEffect, useState} from "react";
+import {getJson} from "../utils/json-requests";
+
 export default function StudentListelement(props) {
+
+  const [student, setStudent] = useState(undefined);
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("")
+
+  useEffect(() => {
+    if (!student) {
+      getJson(props.student).then(res => {
+        setStudent(res.data)
+        setName(res.data.name)
+        setUrl(res.data.url)
+      });
+    }
+  });
+
   return(
     <div id="list-element" className="list-element" style={{textAlign: "left", width: "800px", position: "relative"}}>
 
       <div id="upper-layer">
-        <div id="name" style={{float: "left"}} className="name">Name</div>
+        <div id="name" style={{float: "left"}} className="name">{name}</div>
         <div id="practical-problems" style={{float: "left"}} className="practical-problems">2 practical problems</div>
         <div id="suggestions" style={{float: "right"}} className="suggestions">Suggestions</div>
       </div>
