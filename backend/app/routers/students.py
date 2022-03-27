a732733a8cfe2e3ab9a7a7f2fd6c928dd38c73a8
Vital Wang <vital.wang@hotmail.com>
2022-03-27 19:23:54 +0200

    add rolechecker to students endpoint

--- backend/app/routers/students.py
+++ backend/app/routers/students.py
@@ -1,13 +1,15 @@
 from app.crud import read_where, read_all_where
 from app.models.student import Student, StudentOutSimple, StudentOutExtended
+from app.models.user import UserRole
+from app.utils.checkers import RoleChecker
 from app.utils.response import list_modeltype_response
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from odmantic import ObjectId
 
 router = APIRouter(prefix="/students")
 
 
-@router.get("/", response_description="Students retrieved")
+@router.get("/", dependencies=[Depends(RoleChecker(UserRole.COACH))], response_description="Students retrieved")
 async def get_students():
     """get_students get all the Student instances from the database
 
@@ -18,7 +20,7 @@ async def get_students():
     return list_modeltype_response([StudentOutSimple.parse_raw(r.json()) for r in results], Student)
 
 
-@router.get("/{student_id}", response_description="Student retrieved")
+@router.get("/{student_id}", dependencies=[Depends(RoleChecker(UserRole.COACH))], response_description="Student retrieved")
 async def get_student(student_id):
     """get_student get the Student instances with id from the database
 
