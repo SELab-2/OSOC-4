bbe08b02cea775cbd71d6ec14b092bc36bdffa67
tihabils <tibe.habils@gmail.com>
2022-03-31 00:11:50 +0200

    -ddd.py add extra editions -settings uncomment EditionDropdownButton.js -EditionDropdownmenu fetch data according to new output of /editions endpoint

--- frontend_nextjs/Components/settings/EditionDropdownButton.js
+++ frontend_nextjs/Components/settings/EditionDropdownButton.js
@@ -5,24 +5,24 @@ import {log} from "../../utils/logger";
 
 export default function EditionDropdownButton() {
     const [editionList, setEditionList] = useState([]);
-    const [currentVersion, setCurrentVersion] = useState(["", ""])
+    const [currentVersion, setCurrentVersion] = useState({uri:"", name:"not selected yet"})
 
     useEffect(() => {
         if (editionList.length === 0) {
-            getJson("/editions").then(async res => {
-                log("load editions")
-                log(res)
-                //TODO sort edition list
-                for(let e of res.data){
-                    log("NIETVEELZIEN")
-                    getJson(e.id).then(async edition => {
-                        log(edition.data)
-                        log("wtf")
-                        if(edition.data){
-                            await setEditionList(prevState => [...prevState, edition.data])
-                        }
-                    })
-                }
+            getJson("/editions").then( async editions => {
+                log("Loading editions")
+                log(editions)
+                //TODO find cleaner way for this
+                editions.data.sort((a, b) => {
+                    if (a["name"] > b["name"]) {
+                        return -1
+                    }
+                    if (a["name"] < b["name"]) {
+                        return 1
+                    }
+                    return 0
+                })
+                setEditionList(editions.data)
             })
         }
     })
@@ -31,18 +31,17 @@ export default function EditionDropdownButton() {
         log("change edition dropdown")
         log(item)
         //todo add this in backend? or cookie or other form of local storage?
-        await setCurrentVersion([item.id, item.name]);
+        await setCurrentVersion(item);
     }
 
     return (
         <Dropdown>
             <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic">
-                Current versions name: {currentVersion[1]}
+                Current versions name: {currentVersion.name}
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 {(editionList.length) ? (editionList.map((item, index) => (
-                    //TODO show selected version in menu and make onClick clean or use other method
-                    <Dropdown.Item  onClick={() => ChangeSelectedVersion(item)} key={index} value={item.id}>{item.name}</Dropdown.Item>
+                    <Dropdown.Item  onClick={() => ChangeSelectedVersion(item)} key={index} value={item.uri}>{item.name}</Dropdown.Item>
                 ))) : null}
             </Dropdown.Menu>
         </Dropdown>
