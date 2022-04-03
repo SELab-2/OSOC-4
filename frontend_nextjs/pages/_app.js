ebbaaacd6d71a9ca879e7ae75bc6faec191a472a
SimonVandeVyver <99675815+SimonVandeVyver@users.noreply.github.com>
2022-04-03 10:35:24 +0200

    removed stickyness from navbar

--- frontend_nextjs/pages/_app.js
+++ frontend_nextjs/pages/_app.js
@@ -7,7 +7,6 @@ import "../styles/settingcards.css"
 import "../styles/manageusers.css"
 import "../styles/studentListelement.css"
 import "../styles/select-students.css"
-import "../styles/navbar.css"
 
 import { SessionProvider } from 'next-auth/react';
 import { useState } from 'react';
