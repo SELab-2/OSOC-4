e276f7779f342476b1dfb069bbeee7e5c1e71661
Simon Van de Vyver <simon.vandevyver@ugent.be>
2022-04-06 13:09:14 +0200

    got an error/bug

--- frontend_nextjs/Components/NavHeader.js
+++ frontend_nextjs/Components/NavHeader.js
@@ -1,4 +1,4 @@
-import {Navbar, Container, Col} from 'react-bootstrap';
+import {Navbar, Col, Row} from 'react-bootstrap';
 import { signOut } from 'next-auth/react';
 import Image from 'next/image'
 import osocEmblem from '../public/assets/osoc-emblem.svg';
@@ -16,32 +16,29 @@ export default function NavHeader(props) {
     }
 
     return (
-        <Navbar collapseOnSelect className="navheader">
-            <Container>
-                <Col>
-                    <Navbar.Brand href="/">
-                        <Image className="d-inline-block align-top" src={osocEmblem} alt="osoc-logo" width="65px" height="50px" objectFit={'contain'} />
-                    </Navbar.Brand>
-                </Col>
-                <Col />
+        <Row>
+            <Col>
+                <Navbar.Brand href="/">
+                    <Image className="d-inline-block align-top" src={osocEmblem} alt="osoc-logo" width="65px" height="50px" objectFit={'contain'} />
+                </Navbar.Brand>
+            </Col>
+            <Col />
 
-                <Col md="auto" className="navbar-item" onClick={() => loadpage('/select-students')}>
-                    <p>Select Students</p>
-                </Col>
-                <Col md="auto" className="navbar-item" onClick={() => loadpage('/email-students')}>
-                    <p>Email students</p>
-                </Col>
-                <Col md="auto" className="navbar-item" onClick={() => loadpage('/projects')}>
-                    <p>Projects</p>
-                </Col>
-                <Col md="auto" className="navbar-item" onClick={() => loadpage('/settings')}>
-                    <p>Settings</p>
-                </Col>
-                <Col md="auto" className="navbar-item" onClick={logoutHandler}>
-                    <p>Logout</p>
-                </Col>
-
-            </Container>
-        </Navbar>
+            <Col md="auto" className="navbar-item" onClick={() => loadpage('/select-students')}>
+                <p>Select Students</p>
+            </Col>
+            <Col md="auto" className="navbar-item" onClick={() => loadpage('/email-students')}>
+                <p>Email students</p>
+            </Col>
+            <Col md="auto" className="navbar-item" onClick={() => loadpage('/projects')}>
+                <p>Projects</p>
+            </Col>
+            <Col md="auto" className="navbar-item" onClick={() => loadpage('/settings')}>
+                <p>Settings</p>
+            </Col>
+            <Col md="auto" className="navbar-item" onClick={logoutHandler}>
+                <p>Logout</p>
+            </Col>
+        </Row>
     )
 }
