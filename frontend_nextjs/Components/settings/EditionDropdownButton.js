1ab556d20ba360b14caeaaad2bac8773473f9a5b
tihabils <tibe.habils@gmail.com>
2022-04-06 19:27:54 +0200

    Change edition to new endpoint

--- frontend_nextjs/Components/settings/EditionDropdownButton.js
+++ frontend_nextjs/Components/settings/EditionDropdownButton.js
@@ -1,25 +1,34 @@
 import {Dropdown} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
-import {getJson} from "../../utils/json-requests";
+import {change_edition, getJson} from "../../utils/json-requests";
 import {log} from "../../utils/logger";
+import {useSession} from "next-auth/react";
 
 export default function EditionDropdownButton() {
     const [editionList, setEditionList] = useState([]);
     const [currentVersion, setCurrentVersion] = useState(["", ""])
+    const { data: session, status } = useSession()
+
 
     useEffect(() => {
         if (editionList.length === 0) {
             getJson("/editions").then(async res => {
-                log("load editions")
+                log("load all editions")
                 log(res)
                 //TODO sort edition list
-                for(let e of res.data){
-                    log("NIETVEELZIEN")
-                    getJson(e.id).then(async edition => {
-                        log(edition.data)
-                        log("wtf")
-                        if(edition.data){
-                            await setEditionList(prevState => [...prevState, edition.data])
+                // TODO fix cleaner way to make sure same edition doesn't get loaded multiple times
+                let temp_list = []
+                for(let e of res){
+                    log("load specific edition")
+                    log(e)
+                    getJson(e).then(async edition => {
+                        log(edition)
+                        if(edition){
+                            temp_list.push(edition)
+                        }
+                    }).then(async _ => {
+                        if(res.length === temp_list.length){
+                            await setEditionList(temp_list)
                         }
                     })
                 }
@@ -30,7 +39,9 @@ export default function EditionDropdownButton() {
     const ChangeSelectedVersion = async (item) => {
         log("change edition dropdown")
         log(item)
-        //todo add this in backend? or cookie or other form of local storage?
+        log(item.year)
+        localStorage.setItem("edition", item.year)
+        change_edition(item.year)
         await setCurrentVersion([item.id, item.name]);
     }
 
@@ -42,7 +53,7 @@ export default function EditionDropdownButton() {
             <Dropdown.Menu>
                 {(editionList.length) ? (editionList.map((item, index) => (
                     //TODO show selected version in menu and make onClick clean or use other method
-                    <Dropdown.Item  onClick={() => ChangeSelectedVersion(item)} key={index} value={item.id}>{item.name}</Dropdown.Item>
+                    <Dropdown.Item  onClick={() => ChangeSelectedVersion(item)} key={index} value={item.uri}>{item.name}</Dropdown.Item>
                 ))) : null}
             </Dropdown.Menu>
         </Dropdown>
