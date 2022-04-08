9432895355c0de18b594da8961a2243eb66e4600
Quinten Vervynck <vervynck.quinten@gmail.com>
2022-04-08 18:31:01 +0200

    another bugfix

--- frontend_nextjs/pages/select-students.js
+++ frontend_nextjs/pages/select-students.js
@@ -11,13 +11,13 @@ import {urlManager} from "../utils/ApiClient";
 export default function SelectStudents(props) {
 
     // These constants are initialized empty, the data will be inserted in useEffect
-    const [students, setStudents] = useState([]);
+    const [students, setStudents] = useState(undefined);
 
     // This function inserts the data in the variables
     const { data: session, status } = useSession()
     useEffect( () => {
         if (session) {
-            if (!students.length) {
+            if (!students) {
                 urlManager.getStudents().then(url => getJson(url).then(res => {
                     setStudents(res);
                 }));
