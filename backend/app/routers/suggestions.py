2096c56276d5a698bcaaf4fbac64eeb0b087ec74
Victor Mylle <victor.mylle@hotmail.com>
2022-04-08 20:21:41 +0000

    endpoint for creating suggestion

--- backend/app/routers/suggestions.py
+++ backend/app/routers/suggestions.py
@@ -1,23 +1,17 @@
-from app.crud import read_all_where, read_where
+from app.crud import read_where
 from app.database import get_session
 from app.exceptions.suggestion_exception import SuggestionNotFound
-from app.models.suggestion import Suggestion, SuggestionExtended
-from app.utils.response import list_modeltype_response
+from app.models.suggestion import (Suggestion, SuggestionCreate,
+                                   SuggestionExtended)
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter(prefix="/suggestions")
 
 
-@router.get("/", response_description="Suggestions retrieved")
-async def get_suggestions():
-    """get_suggestions get all the Suggestion instances from the database
-
-    :return: list of suggestions
-    :rtype: dict
-    """
-    results = await read_all_where(Suggestion)
-    return list_modeltype_response(results, Suggestion)
+@router.post("/create", response_description="Suggestion created")
+async def create_suggestion(suggestion: SuggestionCreate, session: AsyncSession = Depends(get_session)):
+    pass
 
 
 @router.get("/{id}", response_description="Suggestion with id retrieved")
