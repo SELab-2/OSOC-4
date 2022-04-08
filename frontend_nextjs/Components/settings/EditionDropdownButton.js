869a14254f2fe7ccb3b7ac6d1088ceca219bf514
tihabils <tibe.habils@gmail.com>
2022-04-08 17:22:24 +0200

    changes to make EditionDropdownButton.js compatible with urlmanager

--- frontend_nextjs/Components/settings/EditionDropdownButton.js
+++ frontend_nextjs/Components/settings/EditionDropdownButton.js
@@ -2,48 +2,56 @@ import {Dropdown} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import {get_edition, getJson} from "../../utils/json-requests";
 import {log} from "../../utils/logger";
+import {urlManager} from "../../utils/ApiClient";
 
 export default function EditionDropdownButton() {
     const [editionList, setEditionList] = useState([]);
     const [currentVersion, setCurrentVersion] = useState(undefined)
 
+    const [loadingCurrentVersion, setLoadingCurrentVersion] = useState(false)
+    const [loadingEditionList, setLoadingEditionList] = useState(false)
 
     useEffect(() => {
-        if(currentVersion === undefined){
-            get_edition().then(edition => {
-                log(edition.year)
-                setCurrentVersion(edition)
+        if(currentVersion === undefined && ! loadingCurrentVersion){
+            setLoadingCurrentVersion(true);
+            urlManager.getCurrentEdition().then(url => {
+                getJson(url).then(edition => {
+                    log(edition)
+                    setCurrentVersion({"year": edition.year, "name": edition.name})
+                    setLoadingCurrentVersion(false)
+                })
             })
         }
 
-        if (editionList.length === 0) {
-            getJson("/editions").then(async res => {
-                log("load all editions")
-                log(res)
-                // TODO fix cleaner way to make sure same edition doesn't get loaded multiple times
-                let temp_list = []
-                for(let e of res){
-                    log("load specific edition")
-                    log(e)
-                    getJson(e).then(async edition => {
-                        log(edition)
-                        if(edition){
-                            temp_list.push(edition)
-                        }
-                    }).then(async _ => {
-                        if(res.length === temp_list.length){
-                            await setEditionList(temp_list)
-                        }
-                    })
-                }
+        if (editionList.length === 0 && ! loadingEditionList) {
+            setLoadingEditionList(true)
+            urlManager.getEditions().then(url => {
+                log("this is the url thing")
+                log(url)
+                getJson(url).then(async res => {
+                    log("load all editions")
+                    log(res)
+                    for(let e of res){
+                        log("load specific edition")
+                        log(e)
+                        getJson(e).then(async edition => {
+                            if(edition){
+                                await setEditionList(prevState => [...prevState, {"year": edition.year, "name": edition.name}])
+                            }
+                        }).then(async () => {
+                            setLoadingEditionList(false)
+
+                        })
+                    }
+                })
             })
         }
-    })
+    }, [currentVersion, loadingEditionList, editionList.length, loadingCurrentVersion])
 
     const ChangeSelectedVersion = async (edition) => {
         log("change edition dropdown")
         log(edition)
-        localStorage.setItem("edition", JSON.stringify({"year": edition.year, "name": edition.name}))
+        await urlManager._setCurrentEdition(edition.year)
         await setCurrentVersion({"year": edition.year, "name": edition.name});
     }
 
