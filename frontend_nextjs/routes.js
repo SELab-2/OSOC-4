cf0323d064f1b8b957bc5d1dde8cc754d23a9243
Simon Van de Vyver <simon.vandevyver@ugent.be>
2022-04-08 13:35:24 +0200

    removed unnecessary routes

--- frontend_nextjs/routes.js
+++ frontend_nextjs/routes.js
@@ -1,10 +1,4 @@
-export const getSkillsPath = () => '/roles'
-
 export const getStudentsPath = () => '/students/'
-
-export const getSuggestionsPath = () => '/suggestions/'
-
-export const getQuestionAnswersPath = () => '/question_answers/'
 
 export const getStudentPath = (id) => '/students/' + id
 
