4961f7839db1ea78731cc456f8bc32a0b82cd4b4
Quinten Vervynck <vervynck.quinten@gmail.com>
2022-04-17 21:29:14 +0200

    added server-side rendering for the dashboard

--- frontend_nextjs/pages/index.js
+++ frontend_nextjs/pages/index.js
@@ -4,14 +4,23 @@ import styles from '../styles/Home.module.css'
 import { useSession } from "next-auth/react"
 import LoadingPage from "../Components/LoadingPage"
 import {useEffect} from "react";
+import {api, Url} from "../utils/ApiClient";
 
-function Home(props) {
-
+function Home({ current_edition, students_length, projects_length }) {
   const { data: session, status } = useSession({ required: true })
   const isUser = session?.user
 
+  console.log("cool")
+  console.log(current_edition);
+
   if (isUser) {
-    return (<h1>COOL DASHBOARD</h1>)
+    return (
+        <>
+          <h1>{current_edition.name}</h1>
+          <h5>There are {projects_length} projects, and {students_length} students!</h5>
+        </>
+    )
+
   }
 
   // Session is being fetched, or no user.
@@ -20,3 +29,25 @@ function Home(props) {
 }
 
 export default Home
+
+export async function getServerSideProps(context) {
+  const current_edition =  await Url.fromName(api.current_edition).get(context)
+  let props_out = {}
+  if (current_edition.success) {
+    props_out["current_edition"] = current_edition.data;
+  }
+
+  const students = await Url.fromName(api.students).get(context)
+  if (students.success) {
+    props_out["students_length"] = students.data.length;
+  }
+
+  const projects = await Url.fromName(api.projects).get(context)
+  if (projects.success) {
+    props_out["projects_length"] = projects.data.length;
+  }
+
+  return {
+    props: props_out
+  }
+}
