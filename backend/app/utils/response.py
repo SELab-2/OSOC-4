b3aaf8348a6553421750cf1ebbd6937d79c053ee
StefVdHaute <vdhstef@hotmail.com>
2022-04-19 01:00:46 +0200

    remove unnecessary dependencies

--- backend/app/utils/response.py
+++ backend/app/utils/response.py
@@ -1,8 +1,3 @@
-from typing import Type
-
-from odmantic.engine import ModelType
-
-
 def response(data, message):
     return {
         "data": data,
@@ -15,7 +10,10 @@ def errorresponse(error, code, message):
     return {"error": error, "code": code, "message": message}
 
 
-def list_modeltype_response(results, model: Type[ModelType]):
-    if results:
-        return response(results, f"Successfully retrieved all instances of {model.__name__}")
-    return response(results, f"No instances of {model.__name__} found, empty list returned")
+def list_modeltype_response(results, model):
+    # deprecated, still uses Mongo
+    # type of Model was Type[odmantic.engine.ModelType]
+    return None
+    # if results:
+    #     return response(results, f"Successfully retrieved all instances of {model.__name__}")
+    # return response(results, f"No instances of {model.__name__} found, empty list returned")
