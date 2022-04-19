a09cd2573cd2ea359a587d0d7fd15ab8a858c3fa
StefVdHaute <vdhstef@hotmail.com>
2022-04-19 14:47:20 +0200

    removed deprecated participations router

--- backend/app/utils/response.py
+++ backend/app/utils/response.py
@@ -8,12 +8,3 @@ def response(data, message):
 
 def errorresponse(error, code, message):
     return {"error": error, "code": code, "message": message}
-
-
-def list_modeltype_response(results, model):
-    # deprecated, still uses Mongo
-    # type of Model was Type[odmantic.engine.ModelType]
-    return None
-    # if results:
-    #     return response(results, f"Successfully retrieved all instances of {model.__name__}")
-    # return response(results, f"No instances of {model.__name__} found, empty list returned")
