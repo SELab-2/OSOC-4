d43e5cc28554e4cda65ad8b8ac7b6edd2e1b5ba6
Simon Van de Vyver <simon.vandevyver@ugent.be>
2022-04-22 16:40:20 +0200

    students in projects tab

--- frontend_nextjs/pages/projects.js
+++ frontend_nextjs/pages/projects.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 import ProjectsList from "../Components/projects/ProjectsList";
-import ProjectCard from "../Components/projects/ProjectCard";
+import StudentListAndFilters from "../Components/select_students/StudentListAndFilters";
 
 export default function Projects() {
 
-    return(
-        <Row className="remaining_height fill_width">
-            <Col className="fill_height">
-                Students
-            </Col>
-            <Col className="fill_height">
-                <ProjectsList/>
-            </Col>
-        </Row>
+  const [students, setStudents] = useState([])
+
+  return(
+    <Row className="remaining_height fill_width">
+      <StudentListAndFilters students={students} setStudents={setStudents} studentsTab={false} studentId={undefined}/>
+
+      <Col className="fill_height">
+        <ProjectsList/>
+      </Col>
+    </Row>
     // Url.fromName(api.edition_projects)
-    )
+  )
 }
--- frontend_nextjs/pages/select-students.js
+++ frontend_nextjs/pages/select-students.js
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from "react";
-import StudentsFilters from "../Components/select_students/StudentsFilters";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import StudentListelement from "../Components/select_students/StudentListelement";
-import { useSession } from "next-auth/react";
-import { api, Url } from "../utils/ApiClient";
 import { useRouter } from "next/router";
 import StudentDetails from "../Components/select_students/StudentDetails";
-import SearchSortBar from "../Components/select_students/SearchSortBar";
-import InfiniteScroll from 'react-infinite-scroll-component';
-import useWindowDimensions from '../utils/WindowDimensions';
-import CheeseburgerMenu from 'cheeseburger-menu'
-import HamburgerMenu from 'react-hamburger-menu'
+import StudentListAndFilters from "../Components/select_students/StudentListAndFilters";
 
 // This function filters the list of students, it is also used in email-students
 export function filterStudents(filterFunctions, students, localFilters, filters, setLocalFilters, setVisibleStudents) {
@@ -33,130 +25,9 @@ export default function SelectStudents() {
 
     // These constants are initialized empty, the data will be inserted in useEffect
     const [students, setStudents] = useState([]);
-    const [studentUrls, setStudentUrls] = useState([]);
 
-    const [visibleStudent, setVisibleStudents] = useState(undefined);
     const studentId = router.query.studentId
 
-    // These variables are used to notice if search or filters have changed, they will have the values of search,
-    // sortby and filters that we filtered for most recently.
-    let [search, setSearch] = useState("");
-    let [sortby, setSortby] = useState("first name+asc,last name+asc");
-    const [decisions, setDecisions] = useState("");
-    const [skills, setSkills] = useState("");
-
-    const [localFilters, setLocalFilters] = useState([0, 0, 0]);
-
-    // These constants represent the filters
-    const filters = [(router.query.filters) ? router.query.filters.split(",") : [],
-    (router.query.skills) ? router.query.skills.split(",") : [],
-    (router.query.decision) ? router.query.decision.split(",") : []]
-
-
-    const [ws, setWs] = useState(undefined);
-  
-    const { data: session, status } = useSession()
-    const { height, width } = useWindowDimensions();
-
-    const [showFilter, setShowFilter] = useState(false);
-
-    /**
-     * This function is called when students, router.query.sortby, router.query.search or filters is changed
-     */
-    useEffect(() => {
-        if (ws) {
-            ws.onmessage = (event) => updateFromWebsocket(event);
-        } else {
-            const new_ws = new WebSocket("ws://localhost:8000/ws")
-            new_ws.onmessage = (event) => updateFromWebsocket(event);
-            setWs(new_ws);
-
-        }
-
-    }, [students, updateFromWebsocket, ws])
-
-    useEffect(() => {
-        if (session) {
-
-            // Check if the search or sortby variable has changed from the search/sortby in the url. If so, update the
-            // variables and update the students list. If the list is updated, we change the local filters. This will
-            // provoke the second part of useEffect to filter the students again.
-            if ((!studentUrls) || (router.query.search !== search) || (router.query.sortby !== sortby) || (router.query.decision !== decisions) || (router.query.skills !== skills)) {
-                setSearch(router.query.search);
-                setSortby(router.query.sortby);
-                setDecisions(router.query.decision);
-                setSkills(router.query.skills);
-
-                // the urlManager returns the url for the list of students
-                Url.fromName(api.editions_students).setParams({ decision: router.query.decision || "", search: router.query.search || "", orderby: router.query.sortby || "", skills: router.query.skills || "" }).get().then(res => {
-                    if (res.success) {
-                        setLocalFilters([0, 0, 0]);
-                        let p1 = res.data.slice(0, 10);
-                        let p2 = res.data.slice(10);
-                        setStudentUrls(p2);
-                        Promise.all(p1.map(studentUrl =>
-                            Url.fromUrl(studentUrl).get().then(res => {
-                                if (res.success) {
-                                    res = res.data;
-                                    Object.values(res["suggestions"]).forEach((item, index) => {
-                                        if (item["suggested_by_id"] === session["userid"]) {
-                                            res["own_suggestion"] = item;
-                                        }
-                                    });
-                                    return res;
-                                }
-                            })
-                        )).then(newstudents => {
-                            console.log(newstudents);
-                            setStudents([...newstudents]);
-                            setLocalFilters([0, 0, 0]);
-                        })
-                    }
-                });
-            }
-            // Check if the real filters have changed from the local filters. If so, update the local filters and
-            // filter the students.
-            // if (students &&
-            //     (localFilters.some((filter, index) => filter !== filters[index].length))) {
-            //     let filterFunctions = [filterStudentsFilters, filterStudentsSkills, filterStudentsDecision];
-            //     filterStudents(filterFunctions, students, localFilters, filters, setLocalFilters, setVisibleStudents);
-            // }
-            // if (filters.every(filter => filter.length === 0)) {
-            //     setVisibleStudents(students);
-            // }
-        }
-    }, [session, students, studentUrls, router.query.search, router.query.sortby, router.query.decision, search, sortby, localFilters, filters])
-
-
-    const updateFromWebsocket = (event) => {
-        let data = JSON.parse(event.data)
-        students.find((o, i) => {
-            if (o["id"] === data["suggestion"]["student_id"]) {
-                let new_students = [...students]
-                new_students[i]["suggestions"][data["id"]] = data["suggestion"];
-                setStudents(new_students);
-                return true; // stop searching
-            }
-        });
-    }
-
-    /**
-     * This function filters the students for the general filters (the filters in filters[0]).
-     * @param filteredStudents The list of students that need to be filtered.
-     * @returns {*} The list of students that are allowed by the general filters.
-     */
-    function filterStudentsFilters(filteredStudents) {
-        return filteredStudents;
-    }
-
-    /**
-     * This function filters the students for the skills filters (the filters in filters[1]).
-     * @param filteredStudents The list of students that need to be filtered.
-     * @returns {*} The list of students that are allowed by the skills filters.
-     */
-    function filterStudentsSkills(filteredStudents) {
-        return filteredStudents;
-    }
 
     function getStudentById() {
         if (students) {
@@ -170,103 +41,19 @@ export default function SelectStudents() {
     }
 
 
-    const fetchData = () => {
-
-        let p1 = studentUrls.slice(0, 10);
-        let p2 = studentUrls.slice(10);
-
-        Promise.all(p1.map(studentUrl =>
-            Url.fromUrl(studentUrl).get().then(res => {
-                if (res.success) {
-                    res = res.data;
-                    Object.values(res["suggestions"]).forEach((item, index) => {
-                        if (item["suggested_by_id"] === session["userid"]) {
-                            res["own_suggestion"] = item;
-                        }
-                    });
-                    return res;
-                }
-            })
-        )).then(newstudents => {
-            setStudents([...students, ...newstudents]);
-            setStudentUrls(p2);
-        })
-    }
-
-
-
     /**
      * The html that displays the overview of students
      */
     return (
-
-        <Row>
-            {((width > 1500) || (width > 1000 && !studentId)) ?
-                <Col md="auto">
-                    <StudentsFilters students={students} filters={filters} />
-                </Col>
-                :
-                <CheeseburgerMenu isOpen={showFilter} closeCallback={() => setShowFilter(false)}>
-                    <StudentsFilters students={students} filters={filters} />
-                </CheeseburgerMenu>
-            }
-            {(width > 800 || !studentId) &&
-
-                <Col className="nomargin student-list-positioning">
-                        <Row className="nomargin">
-                            {!((width > 1500) || (width > 1000 && !studentId)) &&
-                                <Col md="auto">
-                                    <div className="hamburger">
-                                        <HamburgerMenu
-                                            isOpen={showFilter}
-                                            menuClicked={() => setShowFilter(!showFilter)}
-                                            width={18}
-                                            height={15}
-                                            strokeWidth={1}
-                                            rotate={0}
-                                            color='black'
-                                            borderRadius={0}
-                                            animationDuration={0.5}
-                                        />
-                                    </div>
-                                </Col>
-
-                            }
-                            <Col><SearchSortBar /></Col>
-                        </Row>
-                        <Row className="infinite-scroll">
-                            <InfiniteScroll
-                              style={{
-                                  "height": "calc(100vh - 146px)",
-                              }}
-                              dataLength={students.length} //This is important field to render the next data
-                              next={fetchData}
-                              hasMore={studentUrls.length > 0}
-                              loader={<h4>Loading...</h4>}
-                              endMessage={
-                                  <p style={{ textAlign: 'center' }}>
-                                      <b>Yay! You have seen it all</b>
-                                  </p>
-                              }
-                            >
-                                {students.map((i, index) => (
-
-                                  <StudentListelement key={index} student={i} />
-
-                                ))}
-                            </InfiniteScroll>
-                        </Row>
-                </Col>
-
-            }
-
-            {
-                (studentId) &&
-                <Col>
-                    <StudentDetails student={getStudentById()} student_id={studentId} />
-                </Col>
-            }
-        </Row >
+      <Row>
+          <StudentListAndFilters students={students} setStudents={setStudents} studentsTab={true} studentId={studentId}/>
+          {
+            (studentId) &&
+            <Col>
+                <StudentDetails student={getStudentById()} student_id={studentId} />
+            </Col>
+          }
+      </Row >
     )
 
 }
