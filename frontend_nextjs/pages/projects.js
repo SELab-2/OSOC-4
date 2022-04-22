924f7bbc77d512e8a2340a99b53087e0926e51b6
Simon Van de Vyver <simon.vandevyver@ugent.be>
2022-04-22 16:06:35 +0200

    students in projects tab

--- frontend_nextjs/pages/projects.js
+++ frontend_nextjs/pages/projects.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 import ProjectsList from "../Components/projects/ProjectsList";
-import ProjectCard from "../Components/projects/ProjectCard";
+import StudentListAndFilters from "../Components/select_students/StudentListAndFilters";
 
 export default function Projects() {
 
+  const [students, setStudents] = useState([])
+
     return(
         <Row className="remaining_height fill_width">
-            <Col className="fill_height">
-                Students
-            </Col>
+            <StudentListAndFilters students={students} setStudents={setStudents} studentsTab={false} studentId={undefined}/>
+
             <Col className="fill_height">
                 <ProjectsList/>
             </Col>
