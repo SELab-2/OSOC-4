6d7e26fe22ee8b8cb65cd274390d03d6705de1b2
Quinten Vervynck <68075051+QuintenVervynck@users.noreply.github.com>
2022-04-23 13:57:21 +0200

    docs for logger (#304)

--- frontend_nextjs/utils/logger.js
+++ frontend_nextjs/utils/logger.js
@@ -1,4 +1,9 @@
 
 const mode = process.env.NODE_ENV || ""
 
+/**
+ * Logs a message when the mode is development
+ *      for example when the mode is production, this won't log the message
+ * @param msg: the message you want to log
+ */
 export function log(msg) {if (mode ===  "development") console.log(msg);}
