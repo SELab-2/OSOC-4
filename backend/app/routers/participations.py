559450d0a930ed24850745daad24965805e5ecee
Vital Wang <vital.wang@hotmail.com>
2022-04-24 02:02:02 +0200

    participations router

--- backend/app/routers/participations.py
+++ backend/app/routers/participations.py
@@ -1,8 +1,11 @@
+from app.config import config
 from app.crud import read_where, update
 from app.database import get_session
 from app.models.student import Student
+from app.models.project import Project
 from app.models.participation import Participation, ParticipationCreate
 from app.models.user import UserRole
+from app.exceptions.participation_exceptions import ParticipationNotFoundException, InvalidParticipationException
 from app.utils.checkers import EditionChecker, RoleChecker
 from fastapi import APIRouter, Depends
 from fastapi_jwt_auth import AuthJWT
@@ -12,14 +15,36 @@ router = APIRouter(prefix="/participations")
 
 
 @router.post("/create", dependencies=[Depends(RoleChecker(UserRole.COACH))], response_description="Participation created")
-async def create_participation(participation: ParticipationCreate, session: AsyncSession = Depends(get_session), Authorize: AuthJWT = Depends()):
+async def create_participation(participation: ParticipationCreate,
+                               session: AsyncSession = Depends(get_session),
+                               Authorize: AuthJWT = Depends()):
+    """create_participation add a new participation
+
+    :param participation: defaults to Body(...)
+    :type user: ParticipationCreate
+    :return: url of the student of the created participation
+    :rtype: str
+    """
+
+    # check if student and project exists
+    student = await read_where(Student, Student.id == participation.student_id, session=session)
+    if not student:
+        raise InvalidParticipationException()
+
+    project = await read_where(Project, Project.id == participation.project_id, session=session)
+    if not project:
+        raise InvalidParticipationException()
+
+    # check if editions match
+    if student.edition_year != project.edition:
+        raise InvalidParticipationException()
 
     # check edition
-    student = await read_where(Student, Student.id == participation.student_id, session=session)
     await EditionChecker(update=True)(student.edition_year, Authorize, session)
 
     # check if participation already exists
-    old_participation = await read_where(Participation, Participation.project_id == participation.project_id, Participation.student_id == participation.student_id, session=session)
+    old_participation = await read_where(Participation, Participation.project_id == participation.project_id,
+                                         Participation.student_id == participation.student_id, session=session)
 
     if old_participation:
         new_participation_data = participation.dict(exclude_unset=True)
@@ -29,3 +54,84 @@ async def create_participation(participation: ParticipationCreate, session: Asyn
         old_participation = Participation.parse_raw(participation.json())
 
     await update(old_participation, session)
+
+    return f"{config.api_url}students/{old_participation.student_id}"
+
+
+@router.delete("", dependencies=[Depends(RoleChecker(UserRole.COACH))], response_description="Participation deleted")
+async def delete_participation(student_id: int,
+                               project_id: int,
+                               session: AsyncSession = Depends(get_session),
+                               Authorize: AuthJWT = Depends()):
+    """delete_participations remove a participation
+
+    :param student_id: the student id of the participation
+    :type student_id: int
+    :param project_id: the project id of the participation
+    :type project_id: int
+    """
+
+    # check if participation exists
+    participation = await read_where(Participation, Participation.project_id == project_id, Participation.student_id == student_id, session=session)
+
+    if not participation:
+        raise ParticipationNotFoundException()
+
+    # check edition
+    student = await read_where(Student, Student.id == student_id, session=session)
+    if not student:
+        raise ParticipationNotFoundException()
+    await EditionChecker(update=True)(student.edition_year, Authorize, session)
+
+    await session.delete(participation)
+    await session.commit()
+
+
+@router.patch("", dependencies=[Depends(RoleChecker(UserRole.COACH))], response_description="Participation edited")
+async def edit_participation(student_id: int,
+                             project_id: int,
+                             new_participation: ParticipationCreate,
+                             session: AsyncSession = Depends(get_session),
+                             Authorize: AuthJWT = Depends()):
+    """edit_participation edit a participation
+
+    :param student_id: the student id of the participation
+    :type student_id: int
+    :param project_id: the project id of the participation
+    :type project_id: int
+    :param participation: defaults to Body(...)
+    :type user: ParticipationCreate
+    :return: url of the student of the changed participation
+    :rtype: str
+    """
+
+    # check if participation exists
+    old_participation = await read_where(Participation, Participation.project_id == project_id, Participation.student_id == student_id, session=session)
+
+    if not old_participation:
+        raise ParticipationNotFoundException()
+
+    # check if student and project exists
+    student = await read_where(Student, Student.id == new_participation.student_id, session=session)
+    if not student:
+        raise InvalidParticipationException()
+
+    project = await read_where(Project, Project.id == new_participation.project_id, session=session)
+    if not project:
+        raise InvalidParticipationException()
+
+    # check if editions match
+    if student.edition_year != project.edition:
+        raise InvalidParticipationException()
+
+    # check edition
+    await EditionChecker(update=True)(student.edition_year, Authorize, session)
+
+    # update participation
+    new_participation_data = new_participation.dict(exclude_unset=True)
+    for key, value in new_participation_data.items():
+        setattr(old_participation, key, value)
+
+    await update(old_participation, session)
+
+    return f"{config.api_url}students/{old_participation.student_id}"
