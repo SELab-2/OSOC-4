028f1dcf6504a30056cacae535473c3552ac45ee
Jarne Andries <jarne.andries@gmail.com>
2022-04-24 14:41:28 +0200

    Coaches verwijderd uit edition-settings

--- frontend_nextjs/Components/settings/CurrentEdition.js
+++ frontend_nextjs/Components/settings/CurrentEdition.js
@@ -11,7 +11,6 @@ import EditionDropdownButton from "./EditionDropdownButton";
 export default function CurrentEdition(props) {
     const [loadingEditon, setLoadingEdition] = useState(false);
     const [edition, setEdition] = useState(true);
-    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         if (edition === true && ! loadingEditon){
@@ -23,20 +22,6 @@ export default function CurrentEdition(props) {
                 console.log(res);
                 if (res.success) {
                     setEdition(res.data);
-                    Url.fromUrl(res.data.users).get().then(async res2 => {
-                        if (res2.success){
-                            Promise.all(res2.data.map(u => { Url.fromUrl(u).get().then(async res3 => {
-                                        if (res3.success) {
-                                            const user = res3.data;
-                                            if (user && user.data.role === 0) {
-                                                setUsers(prevState => [...prevState, user.data]);
-                                            }
-                                        }
-                                    })
-                                })
-                            )
-                        }
-                    })
                 }
             });
         }
@@ -50,31 +35,7 @@ export default function CurrentEdition(props) {
             <h1>{edition.name}</h1>
             <p>{(edition.description) ? edition.description : "No description available"}</p>
             <Accordion defaultActiveKey="0">
-                <AccordionItem eventkey="0">
-                    <AccordionHeader>
-                        <h3>Coaches</h3>
-                    </AccordionHeader>
-                    <AccordionBody>
-                        { <Table className={"table-manage-users"}>
-                            <thead>
-                                <th>
-                                    <p>Name</p>
-                                </th>
-                                <th>
-                                    <p>Email</p>
-                                </th>
-                            </thead>
-                            <tbody>
-                            {(users.length) ? (users.map((user, _) => (
-                                <tr>
-                                    <td>{user.name}</td>
-                                    <td>{user.email}</td>
-                                </tr>))) : null}
-                            </tbody>
-                        </Table> }
-                    </AccordionBody>
-                </AccordionItem>
-                <AccordionItem eventKey="1">
+                <AccordionItem eventKey="0">
                     <AccordionHeader>
                         <h3>Question Tags</h3>
                     </AccordionHeader>
@@ -84,7 +45,7 @@ export default function CurrentEdition(props) {
                         </div>
                     </AccordionBody>
                 </AccordionItem>
-                <AccordionItem eventKey="2"> 
+                <AccordionItem eventKey="1"> 
                     <AccordionHeader>
                         <h3>Change edition</h3>
                     </AccordionHeader>
