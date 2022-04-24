35fafdb4c1649e5ff81aeab8ac6c5b4826ade4d5
Quinten Vervynck <68075051+QuintenVervynck@users.noreply.github.com>
2022-04-24 14:58:57 +0200

    Match button (#320)
    * added match button
    
    * remove border
    
    * Update frontend_nextjs/styles/projects.css
    
    Co-authored-by: Tibe Habils <67860990+thabils@users.noreply.github.com>
    
    * Update projects.js
    
    Co-authored-by: Tibe Habils <67860990+thabils@users.noreply.github.com>

--- frontend_nextjs/pages/projects.js
+++ frontend_nextjs/pages/projects.js
@@ -1,19 +1,27 @@
 import React, {useState} from "react";
-import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
+import Image from 'next/image'
 import ProjectsList from "../Components/projects/ProjectsList";
 import StudentListAndFilters from "../Components/select_students/StudentListAndFilters";
+import matchIcon from "/public/assets/arrow-right-svgrepo-com.svg"
 
 export default function Projects() {
 
   const [students, setStudents] = useState([])
 
   return(
-    <Row className="remaining_height fill_width">
-      <StudentListAndFilters students={students} setStudents={setStudents} studentsTab={false} studentId={undefined}/>
-
-      <Col className="fill_height fill_width">
-        <ProjectsList/>
-      </Col>
-    </Row>
+      <>
+          <Row className="remaining_height fill_width">
+              <StudentListAndFilters students={students} setStudents={setStudents} studentsTab={false} studentId={undefined}/>
+              <Col md="auto" style={{ marginLeft: "0" }}>
+                  <div style={{paddingTop: "40vh"}} className="fill_height">
+                      <div className="button-match-student-project">
+                          <Image src={matchIcon} alt="match student to project" width={80}/>
+                      </div>
+                  </div>
+              </Col>
+              <ProjectsList/>
+          </Row>
+      </>
   )
 }
