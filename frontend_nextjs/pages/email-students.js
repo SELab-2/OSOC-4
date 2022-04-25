09f780de070c4300f2e25daa76f06d2c8c15c6b1
Victor Mylle <victor.mylle@hotmail.com>
2022-04-25 22:08:10 +0000

    Created Select all button

--- frontend_nextjs/pages/email-students.js
+++ frontend_nextjs/pages/email-students.js
@@ -11,17 +11,23 @@ export default function EmailStudents() {
   const [students, setStudents] = useState([]);
   const [selectedStudents, setSelectedStudents] = useState([]);
 
+  const selectAll = (event) => {
+    console.log("clicked")
+    setSelectedStudents([...students])
+  }
+
   /**
    * the html that displays the 'email students' tab
    */
   return (
     <>
-
       <Row >
         <StudentListAndFilters students={students} setStudents={setStudents} setSelectedStudents={setSelectedStudents} selectedStudents={selectedStudents} studentsTab={false} category="emailstudents" studentId={undefined} />
       </Row>
-      <Row>
+      <Row style={{ "background-color": "lightgray", "height": "55px" }}>
         <Col>{selectedStudents.length}/{students.length}</Col>
+        <Col><button onClick={selectAll}>SELECT ALL</button></Col>
+        <Col><button>SEND NUDES</button></Col>
       </Row>
 
     </>
