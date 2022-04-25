01741d72234def3376cfdac4129de2a2e3eddd79
Quinten Vervynck <vervynck.quinten@gmail.com>
2022-04-25 08:40:04 +0200

    docstrings message

--- frontend_nextjs/Components/dashboard/Message.js
+++ frontend_nextjs/Components/dashboard/Message.js
@@ -1,19 +1,25 @@
-import {Card, Col, Row} from "react-bootstrap";
+import {Card} from "react-bootstrap";
 
 import React from "react";
 
 
+/**
+ * This component displays a message
+ * @param props
+ * @returns {JSX.Element}
+ */
 export default function Message(props) {
 
 
-    return (<Card style={{ width: '40rem', borderRadius: "0.5em", margin: "15pt"}}>
-        <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{props.subtitle}</Card.Subtitle>
-            <Card.Text>
-                {props.children}
-            </Card.Text>
-        </Card.Body>
-    </Card>)
-
-}
+    return (
+        <Card style={{ width: '40rem', borderRadius: "0.5em", margin: "15pt"}}>
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{props.subtitle}</Card.Subtitle>
+                <Card.Text>
+                    {props.children}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
