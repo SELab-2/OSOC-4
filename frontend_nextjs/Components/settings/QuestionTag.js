0617daa611e1956502075d90759fa78355eaca5d
SimonVandeVyver <99675815+SimonVandeVyver@users.noreply.github.com>
2022-04-26 10:39:39 +0200

    layout questiontags (#317)
    * begin questionTags layout
    
    * questionTag layout half done
    
    * forgot to add an icon
    
    * questiontags frontend delete bug
    
    * questionTags frontend finished
    
    * merge conflicts solved

--- frontend_nextjs/Components/settings/QuestionTag.js
+++ frontend_nextjs/Components/settings/QuestionTag.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Form, Button } from 'react-bootstrap';
 import deleteIcon from '../../public/assets/delete.svg';
+import editIcon from '../../public/assets/edit.svg';
+import saveIcon from '../../public/assets/save.svg';
 import Image from "next/image";
 import {Url} from "../../utils/ApiClient";
 
@@ -12,20 +13,12 @@ import {Url} from "../../utils/ApiClient";
 export default function QuestionTag(props) {
     const [previousTag, setPreviousTag] = useState({});
     const [questionTag, setQuestionTag] = useState({});
-    const [loading, setLoading] = useState(false);
 
     // fetch the question tag (the url is provided in the props)
     useEffect(() => {
-        setLoading(true)
-        Url.fromUrl(props.url).get()
-            .then(res => {
-                if(res.success) {
-                    setPreviousTag(res.data);
-                    setQuestionTag(res.data);
-                }
-            })
-            .then(() => setLoading(false))
-    }, [props.url]);
+        setQuestionTag(props.questionTag);
+        setPreviousTag(props.questionTag);
+    }, []);
 
     const handleChange = (event) => {
         event.preventDefault()
@@ -38,65 +31,88 @@ export default function QuestionTag(props) {
 
     const handleCheckboxChange = (event) => {
         const { name, checked } = event.target;
-        setQuestionTag(prevState => ({
-            ...prevState,
-            [name]: checked
-        }));
+
+        let newQuestionTag = {...previousTag};
+        newQuestionTag[name] = checked;
+        Url.fromUrl(questionTag["url"]).setBody(newQuestionTag).patch().then(res => {
+          setPreviousTag(newQuestionTag);
+        })
+
+        newQuestionTag = {...questionTag};
+        newQuestionTag[name] = checked;
+        setQuestionTag(newQuestionTag);
     }
 
     const deleteTag = (event) => {
-        event.preventDefault()
-        Url.fromUrl(props.url).delete().then(res => {
+        event.preventDefault();
+        Url.fromUrl(questionTag["url"]).delete().then(res => {
             if (res.success) {
-                props.deleteTag(props.url)
+                props.deleteTag(questionTag["url"])
             }
         })
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        Url.fromUrl(props.url).setBody(questionTag).patch().then(res => {
+      let requirements = [questionTag["tag"].length > 0,
+        props.questionTags.every(qt => qt["tag"] !== questionTag["tag"] || qt["url"] === questionTag["url"]),
+        questionTag["question"].length > 0,
+        props.questionTags.every(qt => qt["question"] !== questionTag["question"] || qt["url"] === questionTag["url"])];
+        if (requirements.every(req => req)) {
+          Url.fromUrl(questionTag["url"]).setBody(questionTag).patch().then(res => {
             if (previousTag["tag"] !== questionTag["tag"]) {
-                props.renameTag(props.url, res["data"])
+              props.renameTag(questionTag["url"], res["data"])
             }
             setPreviousTag(questionTag);
-        })
+          })
+          props.setErrorMessage("");
+          props.setEdited(undefined);
+        } else {
+          let messages = ["Tag name must not be empty", "Tag name must be unique",
+            "Question must not be empty", "Question must be unique"];
+          props.setErrorMessage(messages[requirements.indexOf(false)]);
+        }
     }
 
     return (
-        <div>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    {questionTag["mandatory"] ? (
-                        <Form.Control name="tag" type="text" placeholder="Tag" value={questionTag["tag"]} onChange={handleChange} disabled />
-                    ) : (
-                        <Form.Control name="tag" type="text" placeholder="Tag" value={questionTag["tag"]} onChange={handleChange} />
-                    )
-                    }
-                    <Form.Control name="question" type="text" placeholder="Question" value={questionTag["question"]} onChange={handleChange} />
+      <tr key={props.questionTag["url"]}>
+          <td>
+              {(questionTag["mandatory"] || ! props.edited)? (
+                <p>{previousTag["tag"]}</p>
+              ) : (
+                <input name="tag" placeholder="Tag" value={questionTag["tag"]} onChange={handleChange}/>
+              )
+              }
+          </td>
+          <td>
+              {(props.edited)? <input name="question" placeholder="Question" value={questionTag["question"]} onChange={handleChange} />:
+              <p>{previousTag["question"]}</p>}
 
-                    {!questionTag["mandatory"] &&
-                        <div>
-                            <Form.Check
-                                name="showInList"
-                                type="checkbox"
-                                checked={questionTag["showInList"]}
-                                label={"show in studentlist"}
-                                onChange={handleCheckboxChange}
-                            />
-                            <button onClick={deleteTag}>
-                                <Image src={deleteIcon} className="delete-icon" />
-                            </button>
-                        </div>
-                    }
-
-                </Form.Group>
-                {JSON.stringify(questionTag) !== JSON.stringify(previousTag) &&
-                    <Button variant="primary" type="submit">
-                        Save
-                    </Button>
-                }
-            </Form>
-        </div>
+          </td>
+          <td>
+              {(!questionTag["mandatory"]) &&
+                <input name="showInList" type="checkbox" checked={questionTag["showInList"]} onChange={handleCheckboxChange} />
+              }
+          </td>
+          <td>
+              {(!questionTag["mandatory"]) &&
+                ((! props.edited)?
+                <button className="table-button" onClick={(ev) => {
+                    props.setNewQuestionTag(undefined);
+                    props.setEdited(previousTag["url"])}}>
+                  <Image src={editIcon} height="30px"/>
+                </button>
+                :
+                <button className="table-button" onClick={handleSubmit}>
+                  <Image src={saveIcon} height="30px"/>
+                </button>)
+              }
+              {(!questionTag["mandatory"]) &&
+                <button onClick={deleteTag} className="table-button">
+                  <Image src={deleteIcon} height="30px"/>
+                </button>
+              }
+          </td>
+      </tr>
     )
 }
--- frontend_nextjs/Components/settings/QuestionTags.js
+++ frontend_nextjs/Components/settings/QuestionTags.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import QuestionTag from "./QuestionTag";
 import {api, Url} from "../../utils/ApiClient";
-import { Form, Button } from 'react-bootstrap';
+import {Form, Button, Table} from 'react-bootstrap';
+import Image from "next/image";
+import saveIcon from "../../public/assets/save.svg";
+import deleteIcon from "../../public/assets/delete.svg";
 import LoadingPage from "../LoadingPage";
 
 /**
@@ -10,63 +13,131 @@ import LoadingPage from "../LoadingPage";
  */
 export default function QuestionTags(props) {
     const [questionTags, setQuestionTags] = useState([]);
-    const [loading, setLoading] = useState(false);
 
-    const [newTag, setNewTag] = useState("");
+    const [edited, setEdited] = useState(undefined);
+    const [newQuestionTag, setNewQuestionTag] = useState(undefined);
+    const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
-        if (props.reload) {
-            setLoading(true);
-            Url.fromName(api.editions_questiontags).get().then(res => {
-                if (res.success) {
-                    setQuestionTags(res.data);
-                }}).then(() => setLoading(false));
-        }
-    }, [props.reload]);
+        Url.fromName(api.editions_questiontags).get().then(res => {
+          if (res.success) {
+            Promise.all(res.data.map(url => Url.fromUrl(url).get())).then(
+              resq => {
+                let newQuestionTags = resq;
+                for (let i = 0; i < newQuestionTags.length; i ++) {
+                  newQuestionTags[i] = newQuestionTags[i]["data"];
+                  newQuestionTags[i]["url"] = res.data[i];
+                }
+                setQuestionTags(newQuestionTags);
+              }
+            )
+          }});
+    }, []);
 
-    const handleNewTagChange = (event) => {
-        setNewTag(event.target.value);
+    function handleNewTagChange(ev) {
+        let newQuestionTagAdj = {...newQuestionTag};
+        newQuestionTagAdj[ev.target.name] = ev.target.value;
+        setNewQuestionTag(newQuestionTagAdj);
     }
 
     async function submitNewTag(event) {
-        event.preventDefault()
-        Url.fromName(api.editions_questiontags).setBody({"tag": newTag}).post().then(resp => {
-            setQuestionTags([...questionTags, resp["data"]])
-            setNewTag("");
-        })
-
+        event.preventDefault();
+        let requirements = [newQuestionTag["tag"].length > 0,
+            ! questionTags.some(qt => qt["tag"] === newQuestionTag["tag"]),
+            newQuestionTag["question"].length > 0,
+            ! questionTags.some(qt => qt["question"] === newQuestionTag["question"])];
+        if (requirements.every(req => req)) {
+            Url.fromName(api.editions_questiontags).setBody({...newQuestionTag}).post().then(resp => {
+                Url.fromUrl(resp["data"]).setBody({...newQuestionTag}).patch().then(() => {
+                    newQuestionTag["url"] = resp["data"];
+                    setQuestionTags([...questionTags, {...newQuestionTag}]);
+                    setNewQuestionTag(undefined);
+                    setEdited(undefined);
+                })
+            })
+            setErrorMessage("");
+        } else {
+            let messages = ["Tag name must not be empty", "Tag name must be unique",
+                "Question must not be empty", "Question must be unique"];
+            setErrorMessage(messages[requirements.indexOf(false)]);
+        }
     }
 
-    const deleteTag = (url) => {
-        const newArr = questionTags.filter(item => item !== url);
+    function deleteTag(url) {
+        const newArr = questionTags.filter(item => item["url"] !== url);
         setQuestionTags(newArr);
     }
 
-    const renameTag = (url, newurl) => {
-        let index = questionTags.indexOf(url);
+    function renameTag(url, newUrl) {
+        let index = questionTags.map(qt => qt["tag"]).indexOf(url);
+        let newQuestionTags = [...questionTags];
         if (index !== -1) {
-            questionTags[index] = newurl;
+            newQuestionTags[index]["url"] = newUrl;
         }
-        setQuestionTags([...questionTags]);
-    }
-
-    if (loading) {
-        return (<LoadingPage/>)
+        setQuestionTags(newQuestionTags);
     }
 
     return (
-        <div>
-            {questionTags.map(url =>
-                    <QuestionTag key={url} url={url} deleteTag={deleteTag} renameTag={renameTag} />
-            )}
-            <Form onSubmit={submitNewTag}>
-                <Form.Control name="newtag" type="text" placeholder="New tag" value={newTag} onChange={handleNewTagChange} />
-                <Button variant="primary" type="submit">
-                    Create
-                </Button>
+      <div>
+          <Table>
+              <thead>
+              <tr className="table-head">
+                  <th>
+                      <p>Name</p>
+                  </th>
+                  <th>
+                      <p>Question</p>
+                  </th>
+                  <th>
+                      <p>Show in student list</p>
+                  </th>
 
-            </Form>
-        </div>
+              </tr>
+              </thead>
+              <tbody className="email-students-cell">
+                  {questionTags.map(questionTag =>
+                    <QuestionTag key={questionTag["url"]} questionTag={questionTag} questionTags={questionTags}
+                                 deleteTag={deleteTag} renameTag={renameTag}
+                                 setEdited={setEdited} edited={edited === questionTag["url"]}
+                                 setNewQuestionTag={setNewQuestionTag} setErrorMessage={setErrorMessage}/>)}
+                  {(newQuestionTag) &&
+                    <tr key="newQuestionTag">
+                        <td>
+                            <input name="tag" placeholder="Tag" value={newQuestionTag["tag"]}
+                                   onChange={handleNewTagChange}/>
+                        </td>
+                        <td>
+                            <input name="question" placeholder="Question" value={newQuestionTag["question"]}
+                                   onChange={handleNewTagChange}/>
+                        </td>
+                        <td><p/></td>
+                        <td>
+                            <button className="table-button" onClick={submitNewTag}>
+                                <Image src={saveIcon} height="30px"/>
+                            </button>
+                            <button onClick={(ev) => {
+                                setErrorMessage("");
+                                setNewQuestionTag(undefined)}} className="table-button">
+                                <Image src={deleteIcon} height="30px"/>
+                            </button>
+                        </td>
+                    </tr>
+                  }
+                  {(errorMessage) &&
+                    <tr key="error">
+                        <td className="errormessage">{errorMessage}</td>
+                    </tr>
+                  }
+              </tbody>
+          </Table>
+
+          <Button variant="primary" onClick={(ev) => {
+              setEdited(undefined);
+              setNewQuestionTag({"tag": "", "question": "", "mandatory": false, "showInList": false});
+          }}>
+              New question tag
+          </Button>
+      </div>
     )
 
 }
