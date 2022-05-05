b0a02f9868b4df59811b3366ab48b67a800a5ce3
StefVdHaute <vdhstef@hotmail.com>
2022-05-05 22:00:51 +0200

    seperate tests

--- backend/app/tests/test_routers/test_reset_password.py
+++ backend/app/tests/test_routers/test_reset_password.py
@@ -2,16 +2,18 @@ from app.database import db
 from app.models.user import User
 from app.tests.test_base import TestBase, Request, Status
 from app.utils.cryptography import verify_password
-from app.utils.keygenerators import generate_new_reset_password_key
+from app.utils.keygenerators import generate_new_reset_password_key, generate_new_invite_key
 
 
 class TestResetPassword(TestBase):
+    new_pass: str = "ValidPass?!123"
+    body: dict[str, str] = {"password": new_pass, "validate_password": new_pass}
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    async def test_valid_resetkey(self):
+    async def test_valid_resetkey_with_correct_key(self):
         path = "/resetpassword/"
-
         user: User = await self.get_user_by_name("user_admin")
 
         # correct key
@@ -19,42 +21,64 @@ class TestResetPassword(TestBase):
         db.redis.setex(key[0], key[1], user.id)
         await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.SUCCESS)
 
+    async def test_valid_resetkey_with_invalid_id(self):
+        path = "/resetpassword/"
+
         # key with bad user id
         key = generate_new_reset_password_key()
         db.redis.setex(key[0], key[1], "bad_user_id")
         await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.BAD_REQUEST)
+
+    async def test_valid_resetkey_with_user_not_in_db(self):
+        path = "/resetpassword/"
 
         # key without user connected to id
         key = generate_new_reset_password_key()
         db.redis.setex(key[0], key[1], self.bad_id)
         await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.BAD_REQUEST)
 
+    async def test_valid_resetkey_with_invitekey(self):
+        path = "/resetpassword/"
+        user: User = await self.get_user_by_name("user_admin")
+
         # Bad key
-        key = generate_new_reset_password_key()
+        key = generate_new_invite_key()
         bad_key = "I" + key[0][1:]
         db.redis.setex(bad_key, key[1], user.id)
         await self.do_request(Request.GET, f"{path}{bad_key}", expected_status=Status.BAD_REQUEST)
 
-    async def test_use_resetkey(self):
+    async def test_use_resetkey_with_invalid_keys(self):
         path = "/resetpassword/"
 
-        new_pass: str = "ValidPass?!123"
-        body = {"password": new_pass, "validate_password": new_pass}
-
         # Request using invalid reset keys
-        await self.do_request(Request.POST, f"{path}Rohnonotsogood", json_body=body, expected_status=Status.BAD_REQUEST)
-        await self.do_request(Request.POST, f"{path}Iohnoevenworse", json_body=body, expected_status=Status.BAD_REQUEST)
+        await self.do_request(Request.POST, f"{path}Rohnonotsogood", json_body=self.body,
+                              expected_status=Status.BAD_REQUEST)
+        await self.do_request(Request.POST, f"{path}Iohnoevenworse", json_body=self.body,
+                              expected_status=Status.BAD_REQUEST)
 
-        # Test with a bad userid
+    async def test_use_resetkey_with_bad_user_id(self):
+        path = "/resetpassword/"
+
         key = generate_new_reset_password_key()
         db.redis.setex(key[0], key[1], self.bad_id)
-        await self.do_request(Request.POST, f"{path}{key[0]}", json_body=body, expected_status=Status.FORBIDDEN)
+        await self.do_request(Request.POST, f"{path}{key[0]}", json_body=self.body, expected_status=Status.FORBIDDEN)
+
+    async def test_use_resetkey_with_inactive_user(self):
+        path = "/resetpassword/"
 
         # Test with an inactive user
         key = generate_new_reset_password_key()
         user: User = await self.get_user_by_name("user_unactivated_coach")
         db.redis.setex(key[0], key[1], user.id)
-        await self.do_request(Request.POST, f"{path}{key[0]}", json_body=body, expected_status=Status.FORBIDDEN)
+        await self.do_request(Request.POST, f"{path}{key[0]}", json_body=self.body, expected_status=Status.FORBIDDEN)
+
+        # Check passwords aren't changed
+        user = await self.get_user_by_name("user_unactivated_coach")
+        self.assertTrue(verify_password(self.saved_objects["passwords"][user.name], user.password),
+                        "The password of an unactivated user was changed.")
+
+    async def test_use_resetkey_with_bad_validation(self):
+        path = "/resetpassword/"
 
         # create a real key
         key = generate_new_reset_password_key()
@@ -63,25 +87,26 @@ class TestResetPassword(TestBase):
 
         # Test using a wrong validate password
         await self.do_request(Request.POST, f"{path}{key[0]}",
-                              json_body={"password": new_pass, "validate_password": new_pass + "1"},
+                              json_body={"password": self.new_pass, "validate_password": self.new_pass + "1"},
                               expected_status=Status.BAD_REQUEST)
 
-        ##################################
-        # Check passwords aren't changed #
-        ##################################
+        # Check passwords aren't changed
         user = await self.get_user_by_name("user_approved_coach")
         self.assertTrue(verify_password(self.saved_objects["passwords"][user.name], user.password),
-                        "The password was changed after bad requests")
+                        "The password was changed after using a wrong validate_password.")
 
-        #####################
-        # Do a good request #
-        #####################
-        # change to new password
-        await self.do_request(Request.POST, f"{path}{key[0]}", json_body=body, expected_status=Status.SUCCESS)
+    async def test_use_resetkey_correct(self):
+        path = "/resetpassword/"
 
-        ###########################
-        # Check current passwords #
-        ###########################
+        # create a real key
+        key = generate_new_reset_password_key()
         user = await self.get_user_by_name("user_approved_coach")
-        self.assertTrue(verify_password(new_pass, user.password),
-                        "The password wasn't changed after successful request")
+        db.redis.setex(key[0], key[1], int(user.id))
+
+        # change to new password
+        await self.do_request(Request.POST, f"{path}{key[0]}", json_body=self.body, expected_status=Status.SUCCESS)
+
+        # Check current passwords
+        user = await self.get_user_by_name("user_approved_coach")
+        self.assertTrue(verify_password(self.new_pass, user.password),
+                        "The password wasn't changed after a successful request")
--- backend/app/tests/test_routers/test_user_invites.py
+++ backend/app/tests/test_routers/test_user_invites.py
@@ -2,16 +2,28 @@ from app.crud import update, read_where
 from app.database import db
 from app.models.user import User
 from app.tests.test_base import TestBase, Status, Request
-from app.utils.keygenerators import generate_new_invite_key
+from app.utils.keygenerators import generate_new_invite_key, generate_new_reset_password_key
 
 
 class TestUserInvites(TestBase):
+    username = "The NewGuy"
+    email = "The.NewGuy@test.test"
+    password = "ValidPass?!123"
+
+    body = {
+        "name": username,
+        "password": password,
+        "validate_password": password
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    ########################
+    # test_valid_invitekey #
+    ########################
     async def test_valid_invitekey(self):
         path = "/invite/"
-
         new_user: User = await update(User.parse_obj({"email": "new.mail@test.test"}), session=self.session)
 
         # correct key
@@ -19,70 +31,79 @@ class TestUserInvites(TestBase):
         db.redis.setex(key[0], key[1], new_user.id)
         await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.SUCCESS)
 
+    async def test_valid_invitekey_key_invalid_id(self):
+        path = "/invite/"
+
         # key with bad user id
         key = generate_new_invite_key()
         db.redis.setex(key[0], key[1], "bad_user_id")
         await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.BAD_REQUEST)
+
+    async def test_valid_invitekey_key_with_user_not_in_db(self):
+        path = "/invite/"
 
         # key without user connected to id
         key = generate_new_invite_key()
         db.redis.setex(key[0], key[1], self.bad_id)
         await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.BAD_REQUEST)
 
-        # Bad key
-        key = generate_new_invite_key()
-        bad_key = "R" + key[0][1:]
-        db.redis.setex(bad_key, key[1], new_user.id)
-        await self.do_request(Request.GET, f"{path}{bad_key}", expected_status=Status.BAD_REQUEST)
-
-    async def test_invited_user(self):
+    async def test_valid_invitekey_with_reset_key(self):
         path = "/invite/"
 
-        username = "The NewGuy"
-        email = "The.NewGuy@test.test"
-        password = "ValidPass?!123"
-        new_user: User = await update(User.parse_obj({"email": email}), session=self.session)
-        body = {
-            "name": username,
-            "password": password,
-            "validate_password": password
-        }
-        bad_body = {
-            "name": username,
-            "password": password,
-            "validate_password": password + "oeps"
-        }
+        new_user: User = await update(User.parse_obj({"email": "new.mail@test.test"}), session=self.session)
+        key = generate_new_reset_password_key()
+        db.redis.setex(key[0], key[1], new_user.id)
+        await self.do_request(Request.GET, f"{path}{key[0]}", expected_status=Status.BAD_REQUEST)
 
-        ###########
-        # bad key #
-        ###########
+    #####################
+    # test_invited_user #
+    #####################
+    async def test_invited_user_with_uid_not_in_db(self):
+        path = "/invite/"
 
-        bad_key, time_delta = generate_new_invite_key()  # Correct key with user that doesn't exist
-        db.redis.setex(bad_key, time_delta, self.bad_id)
-        await self.do_request(Request.POST, f"{path}{bad_key}", expected_status=Status.FORBIDDEN, json_body=body)
+        key, time_delta = generate_new_invite_key()
+        db.redis.setex(key, time_delta, self.bad_id)
+        await self.do_request(Request.POST, f"{path}{key}", expected_status=Status.FORBIDDEN, json_body=self.body)
 
-        bad_key = "R" + bad_key[1:]  # Correct key with bad identifier
+    async def test_invited_user_with_reset_password_key(self):
+        path = "/invite/"
+        new_user: User = await update(User.parse_obj({"email": self.email}), session=self.session)
+
+        bad_key, time_delta = generate_new_reset_password_key()
         db.redis.setex(bad_key, time_delta, new_user.id)
-        await self.do_request(Request.POST, f"{path}{bad_key}", expected_status=Status.BAD_REQUEST, json_body=body)
+        await self.do_request(Request.POST, f"{path}{bad_key}", expected_status=Status.BAD_REQUEST, json_body=self.body)
 
-        bad_key = generate_new_invite_key()[0]  # key that doesn't exist in the db
-        await self.do_request(Request.POST, f"{path}{bad_key}", expected_status=Status.BAD_REQUEST, json_body=body)
-
-        # Assert that the invites were unsuccessful
+        # Assert that the invite was unsuccessful
         new_user = await read_where(User, User.id == new_user.id, session=self.session)
-        self.check_user(new_user, ["", email.lower(), 0, False, False, True], True)
+        self.check_user(new_user, ["", self.email.lower(), 0, False, False, True], True)
+
+    async def test_invited_user_with_key_not_in_db(self):
+        path = "/invite/"
+
+        key = generate_new_invite_key()[0]  # key that doesn't exist in the db
+        await self.do_request(Request.POST, f"{path}{key}", expected_status=Status.BAD_REQUEST, json_body=self.body)
+
+    async def test_invited_user_with_active_user(self):
+        path = "/invite/"
 
         # Try with activated user
         user = await self.get_user_by_name("user_admin")
-        db.redis.setex(bad_key, time_delta, user.id)
-        await self.do_request(Request.POST, f"{path}{bad_key}", expected_status=Status.BAD_REQUEST, json_body=body)
+        key, time_delta = generate_new_invite_key()
+        db.redis.setex(key, time_delta, user.id)
+        await self.do_request(Request.POST, f"{path}{key}", expected_status=Status.BAD_REQUEST, json_body=self.body)
 
         # Assert that the invite was unsuccessful
         # todo
 
-        ###############
-        # correct key #
-        ###############
+    async def test_invited_user_with_active_user(self):
+        path = "/invite/"
+        new_user: User = await update(User.parse_obj({"email": self.email}), session=self.session)
+
+        bad_body = {
+            "name": self.username,
+            "password": self.password,
+            "validate_password": self.password + "oeps"
+        }
 
         key = generate_new_invite_key()
         db.redis.setex(key[0], key[1], new_user.id)
@@ -91,14 +112,21 @@ class TestUserInvites(TestBase):
         await self.do_request(Request.POST, f"{path}{key[0]}", expected_status=Status.BAD_REQUEST, json_body=bad_body)
         # Assert that the invite was unsuccessful
         new_user = await read_where(User, User.id == new_user.id, session=self.session)
-        self.check_user(new_user, ["", email.lower(), 0, False, False, True], True)
+        self.check_user(new_user, ["", self.email.lower(), 0, False, False, True], True)
+
+    async def test_invited_user(self):
+        path = "/invite/"
+        new_user: User = await update(User.parse_obj({"email": self.email}), session=self.session)
+
+        key = generate_new_invite_key()
+        db.redis.setex(key[0], key[1], new_user.id)
 
         # Try successful
-        await self.do_request(Request.POST, f"{path}{key[0]}", expected_status=Status.SUCCESS, json_body=body)
+        await self.do_request(Request.POST, f"{path}{key[0]}", expected_status=Status.SUCCESS, json_body=self.body)
         # Assert that the invite was successful
         new_user = await read_where(User, User.id == new_user.id, session=self.session)
         # check role
-        self.check_user(new_user, [username, email.lower(), 0, True, False, True], False)
+        self.check_user(new_user, [self.username, self.email.lower(), 0, True, False, True], False)
 
     def check_user(self, user: User, expected_values: list, password_empty):
         user_values: list = [user.name, user.email, user.role,
