d1a671a5bbc1034eaeb61a3b43d66ca4ef19474d
Simon Van de Vyver <simon.vandevyver@ugent.be>
2022-05-11 10:06:21 +0200

    close button stays in right top corner

--- frontend_nextjs/Components/select_students/StudentDetails.js
+++ frontend_nextjs/Components/select_students/StudentDetails.js
@@ -231,27 +231,42 @@ export default function StudentDetails(props) {
                 </div>
             }
 
-
-            <Row className="details-upper-layer nomargin">
+            <Row className="nomargin">
+                <Col xs="auto" className="name_big">
+                    {student["mandatory"] ? student["mandatory"]["first name"] : ""} {student["mandatory"] ? student["mandatory"]["last name"] : ""}
+                </Col>
+                <Col>
+                    <Hint message="Delete the student">
+                        <button className="delete-button" onClick={() => setDeletePopUpShow(true)}>
+                            <Image src={deleteIcon} className="delete-icon" />
+                        </button>
+                    </Hint>
+                </Col>
+                <Col />
+                <Col xs="auto" className="close-button">
+                    <Hint message="Close the details">
+                        <Image onClick={() => hideStudentDetails()} className="d-inline-block align-top"
+                               src={closeIcon} alt="close-icon" width="25px" height="25px" objectFit={'contain'} />
+                    </Hint>
+                </Col>
+            </Row>
+            <Row className="nomargin">
                 <Col md="auto">
-                    <Row className="nomargin">
-                        <Col xs="auto" className="name_big">
-                            {student["mandatory"] ? student["mandatory"]["first name"] : ""} {student["mandatory"] ? student["mandatory"]["last name"] : ""}
-                        </Col>
-                        <Col>
-                            <Hint message="Delete the student">
-                                <button className="delete-button" onClick={() => setDeletePopUpShow(true)}>
-                                    <Image src={deleteIcon} className="delete-icon" />
-                                </button>
-                            </Hint>
-                        </Col>
-                    </Row>
-                    <Row className="nomargin">
+                    <Row className="nomargin" style={{marginBottom: "15px"}}>
                         <ul className="nomargin nopadding">
                             {(student["skills"]) && student["skills"].map((skill, index) =>
                                 <li className="skill" style={{ display: "inline-block" }}
                                     key={index}>{skill["name"].toUpperCase()}</li>)}
                         </ul>
+                    </Row>
+                    <Row md="auto" className="decision nomargin">
+                        <GeneralInfo listelement={false} student={student} decision={getDecisionString(decision)} />
+                    </Row>
+                    <Row md="auto" className="nomargin">
+                        <Button className="send-email-button"
+                                onClick={() => setEmailPopUpShow(true)}>
+                            Send email
+                        </Button>
                     </Row>
                 </Col>
                 <Col />
@@ -270,12 +285,6 @@ export default function StudentDetails(props) {
                         <Col xs="auto" className="nopadding">
                             <Hint message="Suggest no">
                                 <button className="suggest-no-button suggest-button" onClick={() => suggest(0)}>No</button>
-                            </Hint>
-                        </Col>
-                        <Col xs="auto" className="close-button">
-                            <Hint message="Close the details">
-                                <Image onClick={() => hideStudentDetails()} className="d-inline-block align-top"
-                                    src={closeIcon} alt="close-icon" width="42px" height="42px" objectFit={'contain'} />
                             </Hint>
                         </Col>
                     </Row>
@@ -300,17 +309,8 @@ export default function StudentDetails(props) {
                     </Row>
                 </Col>
             </Row>
-            <Row className="remaining-height-details" md="auto" style={{}}>
+            <Row className="nomargin" md="auto" style={{}}>
                 <Col className="fill_height scroll-overflow">
-                    <Row md="auto" className="decision nomargin">
-                        <GeneralInfo listelement={false} student={student} decision={getDecisionString(decision)} />
-                    </Row>
-                    <Row md="auto" className="nomargin">
-                        <Button className="send-email-button"
-                            onClick={() => setEmailPopUpShow(true)}>
-                            Send email
-                        </Button>
-                    </Row>
 
                     <Row md="auto" className="h2-titles student-details-suggestions-line nomargin">
                         <Col md="auto" className="suggestions-title"><h2>Suggestions</h2></Col>
