5cc8330ea5cfd89feeafb6f227e5755b30ba86b4
StefVdHaute <63423344+StefVdHaute@users.noreply.github.com>
2022-05-16 22:39:45 +0200

    remove almost identical EmailStudentListelement (#474)

--- frontend_nextjs/Components/students/SuggestionsCount.js
+++ frontend_nextjs/Components/students/SuggestionsCount.js
@@ -6,15 +6,12 @@ import { Col } from "react-bootstrap";
  * @returns {JSX.Element} An element that renders the counts of the suggestions for a student
  */
 export default function SuggestionsCount(props) {
-
-  console.log(props.ownsuggestion)
-
   /**
    * returns the html representation for the suggestion counts
    */
   return [
-    <Col key="suggestionsYes" className={`suggestionsYes ${props.ownsuggestion && props.ownsuggestion["decision"] == 2 ? "selected" : ""}`} xs="auto">{props.suggestionsYes}</Col>,
-    <Col key="suggestionsMaybe" className={`suggestionsMaybe ${props.ownsuggestion && props.ownsuggestion["decision"] == 1 ? "selected" : ""}`} xs="auto">{props.suggestionsMaybe}</Col>,
-    <Col key="suggestionsNo" className={`suggestionsNo ${props.ownsuggestion && props.ownsuggestion["decision"] == 0 ? "selected" : ""}`} xs="auto">{props.suggestionsNo}</Col>
+    <Col key="suggestionsYes" className={`suggestionsYes ${props.ownsuggestion && props.ownsuggestion["decision"] === 2 ? "selected" : ""}`} xs="auto">{props.suggestionsYes}</Col>,
+    <Col key="suggestionsMaybe" className={`suggestionsMaybe ${props.ownsuggestion && props.ownsuggestion["decision"] === 1 ? "selected" : ""}`} xs="auto">{props.suggestionsMaybe}</Col>,
+    <Col key="suggestionsNo" className={`suggestionsNo ${props.ownsuggestion && props.ownsuggestion["decision"] === 0 ? "selected" : ""}`} xs="auto">{props.suggestionsNo}</Col>
   ]
 }
