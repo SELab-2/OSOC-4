2d69898a40fc0e7c658fc5b9a2285e89018fac36
Quinten Vervynck <vervynck.quinten@gmail.com>
2022-05-17 14:27:10 +0200

    moved dependency

--- backend/app/routers/projects.py
+++ backend/app/routers/projects.py
@@ -45,8 +45,8 @@ async def add_project_data(project: ProjectCreate, session: AsyncSession = Depen
     return response(ProjectOutSimple.parse_raw(new_project.json()), "Project added successfully.")
 
 
-@router.get("/{id}", response_description="Project with id retrieved")
-async def get_project_with_id(id: int, role: RoleChecker(UserRole.COACH) = Depends(), session: AsyncSession = Depends(get_session), Authorize: AuthJWT = Depends()):
+@router.get("/{id}", dependencies=[Depends(RoleChecker(UserRole.COACH))], response_description="Project with id retrieved")
+async def get_project_with_id(id: int, session: AsyncSession = Depends(get_session), Authorize: AuthJWT = Depends()):
     """get_project_with_id get Project instance with id from the database
 
     :return: project with id
