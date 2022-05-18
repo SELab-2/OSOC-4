6a7d7b24a8fa2518f9a2531a645cc9b45b26fbeb
Victor Mylle <victor.mylle@hotmail.com>
2022-05-18 21:27:36 +0000

    Fixed navheader

--- frontend_nextjs/Components/NavHeader.js
+++ frontend_nextjs/Components/NavHeader.js
@@ -1,14 +1,14 @@
-import {Navbar, Nav, Container, OverlayTrigger, Button, Popover, Row} from 'react-bootstrap';
-import {signOut} from 'next-auth/react';
+import { Navbar, Nav, Container, OverlayTrigger, Button, Popover, Row } from 'react-bootstrap';
+import { signOut } from 'next-auth/react';
 import Image from 'next/image'
 import osocEmblem from '../public/assets/osoc-emblem.svg';
 import bell from '../public/assets/bell.svg'
 import bellNotifications from '../public/assets/bell-notification.svg'
-import {useEffect, useState} from "react";
-import {api, Url} from "../utils/ApiClient";
-import {useRouter} from "next/router";
+import { useEffect, useState } from "react";
+import { api, Url } from "../utils/ApiClient";
+import { useRouter } from "next/router";
 import Message from './Message';
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function NavHeader(props) {
     async function logoutHandler(event) {
@@ -33,8 +33,8 @@ export default function NavHeader(props) {
                 if (users.success) {
                     Promise.all(users.data.map(userData => Url.fromUrl(userData.id).get())).then(usersData => {
                         let filtered = usersData.filter(resp => resp.success).map(d => d.data.data)
-                        .filter(user => user.active && !user.approved);
-                    setNewUsers(filtered);
+                            .filter(user => user.active && !user.approved);
+                        setNewUsers(filtered);
                     });
                 }
             });
@@ -45,10 +45,10 @@ export default function NavHeader(props) {
      * returns a list of Elements which are the messages rendered
      * @returns {unknown[]}
      */
-     function showNewUsers() {
+    function showNewUsers() {
         if (newUsers && newUsers.length) {
             return [...newUsers.map(user => {
-                
+
                 async function approveUser() {
                     const res = await Url.fromName(api.users).extend("/" + user.id + "/approve").post()
                     if (res.success) {
@@ -69,7 +69,7 @@ export default function NavHeader(props) {
 
                 return (
                     <Message key={user.id} title={`${user.name} has joined`}
-                             subtitle="You can now approve or revoke the access to the application">
+                        subtitle="You can now approve or revoke the access to the application">
                         <h4>{user.name}</h4>
                         <h4>{user.email}</h4>
                         <Row>
@@ -83,42 +83,42 @@ export default function NavHeader(props) {
 
 
     const menu = [
-        {title: 'Students', path: '/students'},
-        {title: 'Projects', path: '/projects'},
-        {title: 'Settings', path: '/settings'},
+        { title: 'Students', path: '/students' },
+        { title: 'Projects', path: '/projects' },
+        { title: 'Settings', path: '/settings' },
     ]
     const router = useRouter()
 
     const notificationsPopover = (<Popover id="popover-notifications">
-      {showNewUsers()}
-  </Popover>);
+        {showNewUsers()}
+    </Popover>);
 
     return (
         <Navbar expand="md">
             <Container>
                 <Image className="d-inline-block align-top" src={osocEmblem} alt="osoc-logo" width="90px"
-                        height="50px" objectFit={'contain'}/>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    height="50px" objectFit={'contain'} />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         {menu.map(item => {
                             return (
                                 <Link href={item.path}
-                                          className={
-                                              router.pathname === item.path ? "active" : ""
-                                          }>
+                                    className={
+                                        router.pathname === item.path ? "active" : ""
+                                    }>
                                     {item.title}
                                 </Link>
                             )
                         })}
-                        {(role === 2)? 
-                            (newUsers && newUsers.length)?
+                        {(role === 2) ?
+                            (newUsers && newUsers.length) ?
                                 <OverlayTrigger trigger="click" placement="bottom" overlay={notificationsPopover}>
-                                <Button className='image-button' style={{marginTop: "5px"}}><Image src={bellNotifications}/></Button>
-                                </OverlayTrigger> 
-                            : <Button className='image-button' style={{marginTop: "5px"}}><Image src={bell}/></Button> 
-                        : null}
-                        <Link onClick={logoutHandler} id="logout-link">Log out</Link>
+                                    <Button className='image-button' style={{ marginTop: "5px" }}><Image src={bellNotifications} /></Button>
+                                </OverlayTrigger>
+                                : <Button className='image-button' style={{ marginTop: "5px" }}><Image src={bell} /></Button>
+                            : null}
+                        <Nav.Link onClick={logoutHandler} id="logout-link">Log out</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
