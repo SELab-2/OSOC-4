d34d058d1543e7d78fea62f6e5d0d0b092e8b02b
StefVdHaute <vdhstef@hotmail.com>
2022-05-18 14:46:32 +0200

    students tests

--- frontend_nextjs/cypress/integration/app.spec.js
+++ frontend_nextjs/cypress/integration/app.spec.js
@@ -1,26 +1,24 @@
 import {signIn} from 'next-auth/react';
 
 describe('Navigation', () => {
-    beforeEach(async () => {
-        await signIn('credentials',
+    beforeEach(() => {
+        signIn('credentials',
             {redirect: false, email: "user_admin@test.be", password: "Test123!user_admin"});
+        cy.visit("/");
     });
 
     it('should navigate to the students page', () => {
-        cy.visit("/");
         cy.contains("a", "students", {matchCase: false}).click();
         cy.url().should("contain", "students");
     });
 
     // uncaught application error
     it('should navigate to the projects page', () => {
-        cy.visit("/");
         cy.contains("a", "projects", {matchCase: false}).click();
         cy.url().should("contain", "projects");
     });
 
     it('should navigate to the settings page', () => {
-        cy.visit("/");
         cy.contains("a", "settings", {matchCase: false}).click();
         cy.url().should("contain", "settings");
     });
