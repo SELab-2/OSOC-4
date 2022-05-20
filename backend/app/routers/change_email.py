4a146016b8eab17964569033e001260f2e48eb65
Jarne Andries <jarne.andries@gmail.com>
2022-05-20 18:58:48 +0200

    functionaliteit werkt

--- backend/app/routers/change_email.py
+++ backend/app/routers/change_email.py
@@ -3,15 +3,13 @@
 from app.crud import read_where, update
 from app.database import db, get_session
 from app.exceptions.key_exceptions import InvalidChangeKeyException
-from app.exceptions.permissions import NotPermittedException
-from app.exceptions.user_exceptions import (PasswordsDoNotMatchException,
-                                            UserAlreadyActiveException)
-from app.models.user import User, UserInvite
 from app.utils.checkers import check_key
-from app.utils.cryptography import get_password_hash
 from app.utils.response import response
-from fastapi import APIRouter, Depends
+from app.models.user import UserResetEmail
+from fastapi import APIRouter, Depends, Body
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User, UserResetPassword
+from app.exceptions.permissions import NotPermittedException
 
 router = APIRouter(prefix="/change")
 
@@ -33,3 +31,43 @@ async def valid_changekey(changekey: str, session: AsyncSession = Depends(get_se
         return response(None, "Valid change email key")
     else:
         raise InvalidChangeKeyException()
+
+@router.post("/{changekey}")
+async def use_changekey(changekey: str, data: UserResetEmail = Body(...),
+                       session: AsyncSession = Depends(get_session)) -> dict:
+    """use_changekey uses a changekey, changes the users email
+
+    :param use_changekey: the reset key
+    :type use_changekey: str
+    :param data: the change data (the new email passwords), defaults to Body(...)
+    :type data: UserResetEmail, optional
+    :param session: the session object, defaults to Depends(get_session)
+    :type session: AsyncSession, optional
+    :raises InvalidChangeKeyException: raised when the change key isn't valid
+    :raises NotPermittedException: unauthorized
+    :return: a response message
+    :rtype: dict
+    """
+    print("Hier geraakt")
+    if changekey[0] != "C":
+        raise InvalidChangeKeyException()
+
+    userid = db.redis.get(changekey)  # check that the inv key exists
+
+    if userid:
+        user = await read_where(User, User.id == int(userid), session=session)
+
+        if not user:
+            raise NotPermittedException()
+
+        if not user.active:
+            raise NotPermittedException()
+
+        user.email = data.email
+
+        await update(user, session=session)
+        db.redis.delete(changekey)
+
+        return response(None, "New email successfully set, you can now login with your new email")
+
+    raise InvalidChangeKeyException()
--- frontend_nextjs/pages/change/[changekey].js
+++ frontend_nextjs/pages/change/[changekey].js
@@ -1,14 +1,22 @@
 import { useRouter } from "next/router";
 import React, { useEffect } from 'react'
 import { useState } from 'react';
+import { ToastContainer, toast} from "react-toastify";
 import LoadingPage from "../../Components/LoadingPage"
 import { api, Url } from "../../utils/ApiClient";
+import logoScreen from '../../public/assets/osoc-screen.png';
+import Image from 'next/image'
 
 const ChangeKey = () => {
     const router = useRouter();
     const {changekey} = router.query
     const [validKey, setValidKey] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [email, setEmail] = useState("");
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+    }
 
     useEffect(async () => {
         Url.fromName(api.change).extend(`/${changekey}`).get().then(resp =>{
@@ -20,15 +28,48 @@ const ChangeKey = () => {
         setLoading(false);
     }, [changekey])
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = {"email": email}
+        const resp = await Url.fromName(api.change).extend(`/${changekey}`).setBody(data).post();
+
+        if (resp.success) {
+            toast.success(resp.data["message"]);
+            await router.push('/login');
+        } else {
+            console.log("Niet gelukt");
+            console.log(resp);
+        }
+    }
+
     if (loading) {
         return <LoadingPage />
     }
     else if (!validKey) {
-        return <h1>Not a valid change email key: {changekey}</h1>
+        return <h1>Not a valid change email key</h1>
     }
 
     return (
-        <h1>Gelukt!!!!</h1>
+        <div className='body-login'>
+            <section className="body-left">
+                <div className="image-wrapper">
+                    <Image className="logo" src={logoScreen} alt="osoc-logo" />
+                </div>
+            </section>
+            <section className='body-right'>
+                <div className="login-container">
+                    <p className="welcome-message">Reset your email</p>
+                    <div className="login-form">
+                        <input type="email" name="email" value={email} onChange={handleChangeEmail} placeholder="New email" />
+                        <button className="submit" onClick={handleSubmit}>
+                            Submit
+                        </button>
+
+                    </div>
+                </div>
+            </section>
+            <ToastContainer/>
+        </div>
     )
 
 }
