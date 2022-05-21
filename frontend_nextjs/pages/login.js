38bf3bba52fe0a9511ccc59a0986706208f6f1f5
Jarne Andries <jarne.andries@gmail.com>
2022-05-21 14:51:44 +0200

    added toasts to resetting password

--- frontend_nextjs/pages/login.js
+++ frontend_nextjs/pages/login.js
@@ -5,7 +5,8 @@ import { signIn } from 'next-auth/react';
 import logoScreen from '../public/assets/osoc-screen.png';
 import LoadingPage from "../Components/LoadingPage";
 import {api, Url} from "../utils/ApiClient";
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Spinner} from 'react-bootstrap';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Login = props => {
     const [email, setEmail] = useState("");
@@ -14,6 +15,7 @@ const Login = props => {
     const [emailForgot, setEmailForgot] = useState("")
     const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState("");
+    const [saving, setSaving] = useState(false);
     const router = useRouter();
     // let from = useLocation().state?.from?.pathname || "/";
     // const navigate = useNavigate()
@@ -42,14 +44,23 @@ const Login = props => {
 
     async function handleSubmitForgot(event) {
         event.preventDefault();
+        setSaving(true);
         let credentials = JSON.stringify({
             "email": emailForgot,
         });
-        // post, if any errors, show them
-        let output = Url.fromName(api.forgot).setBody(credentials).post();
-        if (output.success) {
+        const resp = await Url.fromName(api.forgot).setBody(credentials).post().then()
+        if (resp.success) {
+            setSaving(false);
+            toast.success("Email to reset password sent succesfully. Please check your inbox");
+            setEmailForgot("");
+            await setTimeout(function(){
+                setShowForgot(false);
+            }, 4000);
+        } else {
+            setSaving(false);
+            toast.error("Something went wrong, please try again");
+            setEmailForgot("");
         }
-        setShowForgot(false)
     }
 
     if (loading) {
@@ -84,12 +95,24 @@ const Login = props => {
                         <p className="welcome-message">Please provide your email address to proceed</p>
                         <Form onSubmit={handleSubmitForgot}>
                             <Form.Control type="email" name="email" value={emailForgot} onChange={handleChangeEmailForgot} placeholder="Email address" />
-                            <Button className="submit" type="submit">Reset my password</Button>
+                            {saving ? 
+                                <Button type="submit" className="submit" disabled>
+                                Sending reset email...
+                                <Spinner
+                                    as="span"
+                                    animation="border"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                />
+                                </Button> 
+                            : <Button className="submit" type="submit">Reset my password</Button>}
                         </Form>
                         <a href="#" onClick={() => { setShowForgot(false); }} >Just want to log in?</a>
                     </div>
                 )}
             </section>
+            <ToastContainer autoClose={4000}/>
         </div>
     )
 }
