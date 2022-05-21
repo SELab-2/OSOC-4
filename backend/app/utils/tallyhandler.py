652b0048759e2ee3173f6aba8aac30bd3b99c950
victormylle <32961910+victormylle@users.noreply.github.com>
2022-05-21 20:59:52 +0200

    Extract skills from tally form (#578)
    * Extract skills from tally form
    Fixes #565
    
    * Extract skills from tally form
    Fixes #565
    
    * reset ddd
    
    * Reload skills when skill questiontag changed
    Fixes #565
    
    * Translated volgnummer
    
    * Created new issue
    
    * sync QuestionAnswers with Skills for student
    
    * Fixed QuestionAnswerGenerator + delete all studentskills when skill question changed
    
    Co-authored-by: Vital Wang <vital.wang@hotmail.com>

--- backend/app/utils/tallyhandler.py
+++ backend/app/utils/tallyhandler.py
@@ -5,6 +5,8 @@ from app.crud import read_where, update
 from app.models.answer import Answer
 from app.models.question import Question
 from app.models.question_answer import QuestionAnswer
+from app.models.question_tag import QuestionTag
+from app.models.skill import Skill, StudentSkill
 from app.models.student import Student
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -39,19 +41,30 @@ async def process_tally(data: dict[str, str], edition: int, session: AsyncSessio
     student = Student(edition_year=edition)
     await update(student, session=session)
 
+    # get the skills questiontag
+    skills_tag = await read_where(QuestionTag, QuestionTag.tag == "skills", QuestionTag.edition == edition, session=session)
+
+    # using a number for the questionanswers to be able to display them in the correct order
+    number = 0
+
     for field in data["data"]["fields"]:
 
-        if "Which role are you applying for that is not in the list above?" == field["label"]:
-            field["label"] = "Which role are you applying for?"
+        # if "Which role are you applying for that is not in the list above?" == field["label"]:
+        #     field["label"] = "Which role are you applying for?"
 
         # Check if the question already exists else save it
         # get the question by id
         q = await read_where(Question, Question.field_id == field["key"], Question.edition == edition, session=session)
 
         # check if the saved question is the same as the one in the form
-
         if not q:
-            q = Question(question=field["label"], field_id=field["key"], edition=edition)
+            # check if a question is saved without the field id => needed when a question created with a questiontag
+            q = await read_where(Question, Question.question == field["label"], Question.edition == edition, session=session)
+            if q:
+                # if so update the field id
+                q.field_id = field["key"]
+            else:
+                q = Question(question=field["label"], field_id=field["key"], edition=edition)
             await update(q, session=session)
         elif q.question != field["label"]:
             q.question = field["label"]
@@ -64,10 +77,23 @@ async def process_tally(data: dict[str, str], edition: int, session: AsyncSessio
                     if option["id"] in q_values and option["text"] != "Other":
                         a = await get_save_answer(option["text"], session)
 
+                        if q.id == skills_tag.question_id:
+                            # save the skill for the student
+                            skill = await read_where(Skill, Skill.name == option["text"], session=session)
+                            print("found skill")
+                            if not skill:
+                                skill = Skill(name=option["text"])
+                                await update(skill, session=session)
+
+                            student_skill = StudentSkill(student_id=student.id, skill_name=skill.name)
+                            await update(student_skill, session=session)
+
                         # save the question answer binding
-                        await update(QuestionAnswer(student=student, question=q, answer=a), session=session)
+                        await update(QuestionAnswer(student=student, number=number, question=q, answer=a), session=session)
+                        number += 1
 
         elif field["type"] in ["TEXTAREA", "INPUT_TEXT", "INPUT_NUMBER", "INPUT_PHONE_NUMBER", "INPUT_EMAIL", "INPUT_LINK", "FILE_UPLOAD"]:
             if field["value"] is not None:
                 a = await get_save_answer(str(field["value"]), session)
-                await update(QuestionAnswer(student=student, question=q, answer=a), session=session)
+                await update(QuestionAnswer(student=student, number=number, question=q, answer=a), session=session)
+                number += 1
